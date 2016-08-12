e86b9aa55e086e8a5f75ea51eca55d84f2af2168
Christoph Welcz <christoph@welcz.de>
2016-08-12 05:04:06 +0200

    now with sexy list initialization

--- eclipse/FizzBuzz/src/de/welcz/FizzBuzzEngine.java
+++ eclipse/FizzBuzz/src/de/welcz/FizzBuzzEngine.java
@@ -1,16 +1,14 @@
 package de.welcz;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FizzBuzzEngine {
 
-	private static final List<Rule> rules = new ArrayList<Rule>();
-	static {
-		rules.add(Rule.divisibleBy(15).returns("FizzBuzz"));
-		rules.add(Rule.divisibleBy(3).returns("Fizz"));
-		rules.add(Rule.divisibleBy(5).returns("Buzz"));
-	}
+	private static final List<Rule> rules = Arrays.asList(
+			Rule.divisibleBy(15).returns("FizzBuzz"),
+			Rule.divisibleBy(3).returns("Fizz"), 
+			Rule.divisibleBy(5).returns("Buzz"));
 
 	public String calculateNext(int number) {
 		return rules.stream()
