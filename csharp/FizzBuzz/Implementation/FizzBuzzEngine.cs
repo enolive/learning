d2acf47bffd2b11b148b24dc8e6247e154689d2c
Christoph Welcz <christoph@welcz.de>
2016-10-16 18:36:32 +0200

    CSharp project and some cleanup

---
+++ csharp/FizzBuzz/Implementation/FizzBuzzEngine.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace Implementation
+{
+    public class FizzBuzzEngine
+    {
+        public string Calculate(int number)
+        {
+            if (IsDivisibleBy(number, 3))
+            {
+                return "Fizz";
+            }
+
+            return number.ToString(CultureInfo.InvariantCulture);
+        }
+
+        private static bool IsDivisibleBy(int number, int denominator)
+        {
+            return number % denominator == 0;
+        }
+    }
+}
---
+++ csharp/FizzBuzz/Tests/FizzBuzzTest.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Implementation;
+using Xunit;
+
+namespace Tests
+{
+    public class FizzBuzzTest
+    {
+        private readonly FizzBuzzEngine _target;
+
+        public FizzBuzzTest()
+        {
+            _target = new FizzBuzzEngine();
+        }
+
+        [Theory]
+        [InlineData(0, "0")]
+        [InlineData(1, "1")]
+        public void CalculcateShouldReturnNormalNumbersAsIs(int number, string expectedResult)
+        {
+            // act
+            var result = _target.Calculate(number);
+
+            // assert
+            result.Should().Be(expectedResult);
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(6)]
+        public void CalculateShouldReturnFizz(int number)
+        {
+            // act
+            var result = _target.Calculate(number);
+
+            // assert
+            result.Should().Be("Fizz");
+        }
+    }
+}
