c461491b73d3c26538cf5fbff5743d02b5bb0a67
Christoph Welcz <noreply@panzerpunze.de>
2016-11-10 00:53:13 +0100

    Just some experiments with traits

--- scala/RomanNumbers/src/main/scala/RomanDigit.scala
+++ scala/RomanNumbers/src/main/scala/RomanDigit.scala
@@ -1,14 +1,7 @@
 
-case class RomanDigit(digit: String) {
-  def toArabic: Int = {
-    digit match {
-      case "I" => 1
-      case "V" => 5
-      case "X" => 10
-      case "L" => 50
-      case "C" => 100
-      case "D" => 500
-      case "M" => 1000
+case class RomanDigit(digit: Char) extends ConvertToArabic {
+    def toArabic: Int = {
+        toArabic(digit)
     }
-  }
+
 }
---
+++ scala/RomanNumbers/src/test/scala/AllDigitsTest.scala
@@ -0,0 +1,15 @@
+import org.scalatest.{FlatSpec, Matchers}
+
+class AllDigitsTest extends FlatSpec with Matchers {
+    behavior of "Roman digits"
+
+    it should "have expected first digit" in {
+        val digits = AllDigits.of("X")
+        digits should contain only RomanDigit('X')
+    }
+
+    it should "have expected two digits" in {
+        val digits = AllDigits.of("XII")
+        digits should contain allElementsOf List(RomanDigit('X'), RomanDigit('I'), RomanDigit('I'))
+    }
+}
