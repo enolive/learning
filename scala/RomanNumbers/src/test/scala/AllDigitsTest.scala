72c455248317d48477c4fc0f4b91ec078ed274e8
Christoph Welcz <noreply@panzerpunze.de>
2016-12-26 15:46:05 +0100

    Minor test refactorings

--- scala/RomanNumbers/src/test/scala/AllDigitsTest.scala
+++ scala/RomanNumbers/src/test/scala/AllDigitsTest.scala
@@ -5,11 +5,15 @@ class AllDigitsTest extends FlatSpec with Matchers {
 
     it should "have expected first digit" in {
         val digits = AllDigits.of("X")
-        digits should contain only new RomanDigit('X')
+        digits should contain only romanDigit('X')
     }
 
     it should "have expected three digits" in {
         val digits = AllDigits.of("XII")
-        digits should contain allElementsOf List(new RomanDigit('X'), new RomanDigit('I'), new RomanDigit('I'))
+        digits should contain allElementsOf List(romanDigit('X'), romanDigit('I'), romanDigit('I'))
+    }
+
+    private def romanDigit(digit: Char) = {
+        new RomanDigit(digit)
     }
 }
--- scala/RomanNumbers/src/test/scala/RomanDigitTest.scala
+++ scala/RomanNumbers/src/test/scala/RomanDigitTest.scala
@@ -4,34 +4,34 @@ class RomanDigitTest extends FlatSpec with Matchers {
     behavior of "Converting roman digits to arabic"
 
     it should "convert I to 1" in {
-        theArabicRepresentationOf('I') should be(1)
+        arabicValueOf('I') should be(1)
     }
 
     it should "convert V to 5" in {
-        theArabicRepresentationOf('V') should be(5)
+        arabicValueOf('V') should be(5)
     }
 
     it should "convert X to 10" in {
-        theArabicRepresentationOf('X') should be(10)
+        arabicValueOf('X') should be(10)
     }
 
     it should "convert L to 50" in {
-        theArabicRepresentationOf('L') should be(50)
+        arabicValueOf('L') should be(50)
     }
 
     it should "convert C to 100" in {
-        theArabicRepresentationOf('C') should be(100)
+        arabicValueOf('C') should be(100)
     }
 
     it should "convert D to 500" in {
-        theArabicRepresentationOf('D') should be(500)
+        arabicValueOf('D') should be(500)
     }
 
     it should "convert M to 1000" in {
-        theArabicRepresentationOf('M') should be(1000)
+        arabicValueOf('M') should be(1000)
     }
 
-    private def theArabicRepresentationOf(digit: Char): Int = {
+    private def arabicValueOf(digit: Char): Int = {
         new RomanDigit(digit).toArabic
     }
 }
