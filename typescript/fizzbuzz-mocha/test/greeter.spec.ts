160e52a84b3c4d00a1175dd6061c9d357d590d8c
Christoph Welcz <christoph@welcz.de>
2017-05-31 11:48:45 +0200

    First Test

---
+++ typescript/fizzbuzz-mocha/test/greeter.spec.ts
@@ -0,0 +1,8 @@
+import {expect} from 'chai';
+
+describe('Greeter', () => {
+    it('should greet the world', () => {
+        let result = 'Hello World!';
+        expect(result).to.equal('Hello World!');
+    });
+});
