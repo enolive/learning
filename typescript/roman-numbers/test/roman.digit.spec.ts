5292e50c38650f6003249771e083917dcbfed09f
Christoph Welcz <christoph@welcz.de>
2017-05-31 20:55:02 +0200

    made digit test use only the digit

--- typescript/roman-numbers/test/roman.digit.spec.ts
+++ typescript/roman-numbers/test/roman.digit.spec.ts
@@ -1,18 +1,19 @@
-import {ConvertRoman} from "../src/roman.converter";
 import {expect} from "chai";
+import {RomanDigit} from "../src/roman.digit";
 
 describe("Single Roman Digit", () => {
     it("should be converted", () => {
-        expect(ConvertRoman.toArabic("I")).to.equal(1);
-        expect(ConvertRoman.toArabic("V")).to.equal(5);
-        expect(ConvertRoman.toArabic("X")).to.equal(10);
-        expect(ConvertRoman.toArabic("L")).to.equal(50);
-        expect(ConvertRoman.toArabic("C")).to.equal(100);
-        expect(ConvertRoman.toArabic("D")).to.equal(500);
-        expect(ConvertRoman.toArabic("M")).to.equal(1000);
+        expect(new RomanDigit("I").toArabic()).to.equal(1);
+        expect(new RomanDigit("V").toArabic()).to.equal(5);
+        expect(new RomanDigit("X").toArabic()).to.equal(10);
+        expect(new RomanDigit("L").toArabic()).to.equal(50);
+        expect(new RomanDigit("C").toArabic()).to.equal(100);
+        expect(new RomanDigit("D").toArabic()).to.equal(500);
+        expect(new RomanDigit("M").toArabic()).to.equal(1000);
     });
 
     it("should throw on invalid", () => {
-        expect(() => ConvertRoman.toArabic("R")).to.throw(RangeError, "Invalid roman digit 'R'.");
+        expect(() => new RomanDigit("R").toArabic())
+            .to.throw(RangeError, "Invalid roman digit 'R'.");
     });
 });
