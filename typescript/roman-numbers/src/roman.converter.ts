cfb4fbb864e1634d846f60a7a085855362510feb
Christoph Welcz <christoph@welcz.de>
2017-05-31 18:22:09 +0200

    exception test and some grouping

--- typescript/roman-numbers/src/roman.converter.ts
+++ typescript/roman-numbers/src/roman.converter.ts
@@ -1,15 +1,26 @@
 export class RomanConverter {
 
+    //noinspection TsLint - ordering is not appropriate here
+    private digitMap = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    };
+
     public toArabic(roman: string): number {
         const character = roman[0];
-        //noinspection TsLint
-        const digits = {
-            I: 1,
-            V: 5,
-            X: 10,
-            L: 50,
-        };
+        return this.convertDigit(character);
+    }
 
-        return digits[character];
+    private convertDigit(character: string): number {
+        const digit = this.digitMap[character];
+        if (!digit) {
+            throw RangeError(`Invalid roman digit '${character}'.`);
+        }
+        return digit;
     }
 }
--- typescript/roman-numbers/test/roman.converter.spec.ts
+++ typescript/roman-numbers/test/roman.converter.spec.ts
@@ -6,11 +6,19 @@ describe("Roman to Arabic converter", () => {
 
     beforeEach(() => target = new RomanConverter());
 
-    it("should convert single digits", () => {
-        expect(target.toArabic("I")).to.equal(1);
-        expect(target.toArabic("V")).to.equal(5);
-        expect(target.toArabic("X")).to.equal(10);
-        expect(target.toArabic("L")).to.equal(50);
-    });
+    describe("single digits", () => {
+        it("should be converted", () => {
+            expect(target.toArabic("I")).to.equal(1);
+            expect(target.toArabic("V")).to.equal(5);
+            expect(target.toArabic("X")).to.equal(10);
+            expect(target.toArabic("L")).to.equal(50);
+            expect(target.toArabic("C")).to.equal(100);
+            expect(target.toArabic("D")).to.equal(500);
+            expect(target.toArabic("M")).to.equal(1000);
+        });
 
+        it("should throw on invalid", () => {
+            expect(() => target.toArabic("R")).to.throw(RangeError, "Invalid roman digit 'R'.");
+        });
+    });
 });
