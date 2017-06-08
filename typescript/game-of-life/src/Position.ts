bc206d23a6388ad0889cb21d52d3a1a72bd9faec
Christoph Welcz <christoph@welcz.de>
2017-06-08 16:05:12 +0200

    optimize hash to use XOR instead of add

--- typescript/game-of-life/src/Position.ts
+++ typescript/game-of-life/src/Position.ts
@@ -1,6 +1,6 @@
 export class Position {
     public static hash(position: Position) {
-        return 37 * position.x + position.y;
+        return (37 * position.x) ^ position.y;
     }
 
     constructor(private x: number, private y: number) {
