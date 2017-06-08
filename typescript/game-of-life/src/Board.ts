b4a7bd794e1dd4471ea6c68144d6f0037b0be4ba
Christoph Welcz <christoph@welcz.de>
2017-06-08 15:58:14 +0200

    hash set instead of list for living cells

--- typescript/game-of-life/src/Board.ts
+++ typescript/game-of-life/src/Board.ts
@@ -1,13 +1,15 @@
+import {Set} from "hash-set-map";
 import {Position} from "./Position";
 
 export class Board {
-    private wasSetAlive: Position[] = [];
+
+    private livingCells = new Set(Position.hash);
 
     public isCellAliveAt(position: Position) {
-        return this.wasSetAlive.some((p) => p.equals(position));
+        return this.livingCells.has(position);
     }
 
     public setCellAliveAt(position: Position) {
-        this.wasSetAlive.push(position);
+        this.livingCells.add(position);
     }
 }
--- typescript/game-of-life/src/Position.ts
+++ typescript/game-of-life/src/Position.ts
@@ -1,12 +1,8 @@
 export class Position {
-    constructor(private x: number, private y: number) {
+    public static hash(position: Position) {
+        return 37 * position.x + position.y;
     }
 
-    public equals(other: Position): boolean {
-        if (other == null) {
-            return false;
-        }
-
-        return other.x === this.x && other.y === this.y;
+    constructor(private x: number, private y: number) {
     }
 }
