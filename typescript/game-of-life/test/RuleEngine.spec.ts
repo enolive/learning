45026feb98369ba3b25f487b0d5c94438d9ecdcf
Christoph Welcz <christoph@welcz.de>
2017-06-08 10:51:35 +0200

    First test

--- typescript/game-of-life/test/RuleEngine.spec.ts
+++ typescript/game-of-life/test/RuleEngine.spec.ts
@@ -1,3 +1,24 @@
-/**
- * Created by eno-live on 08.06.17.
- */
+import {expect} from "chai";
+
+export class Engine {
+
+    nextState(current: CellState, livingNeighbours: number) {
+        return CellState.Dead;
+    }
+}
+
+export enum CellState {
+    Dead,
+    Living
+
+}
+
+describe("Rules of Game of Life", () => {
+   it("should cell with less than 2 living neighbours die", () => {
+       const engine = new Engine();
+       const state = CellState.Living;
+       const livingNeighbours = 1;
+       const nextState = engine.nextState(state, livingNeighbours);
+       expect(nextState).to.equal(CellState.Dead);
+   })
+});
