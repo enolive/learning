74fac97110616e28c26229aaac164b4738a4e31e
Christoph Welcz <christoph@welcz.de>
2017-06-09 17:17:16 +0200

    finished cartesian product in FP -- yay!!!

--- typescript/game-of-life/src/Dimensions.ts
+++ typescript/game-of-life/src/Dimensions.ts
@@ -1,8 +1,15 @@
 import {Position} from "./Position";
+import * as _ from "lodash";
 
 export class Dimensions {
     private _width: number;
     private _height: number;
+
+    public static cartesianProduct(width: number, height: number): number[][] {
+        const xRange = _.range(this.positiveOrZero(width));
+        const yRange = _.range(this.positiveOrZero(height));
+        return _.flatMap(xRange, (x) => _.map(yRange, (y) => [x, y]));
+    }
 
     public constructor(width: number, height: number) {
         this._width = width;
@@ -10,13 +17,12 @@ export class Dimensions {
     }
 
     public positions() {
-            const positions = [];
-            for (let x = 0; x < this._width; x++) {
-                for (let y = 0; y < this._height; y++) {
-                    const position = new Position(x, y);
-                    positions.push(position);
-                }
-            }
-            return positions;
+        return Dimensions
+            .cartesianProduct(this._width, this._height)
+            .map(([x, y]) => new Position(x, y));
+    }
+
+    private static positiveOrZero(number: number) {
+        return number >= 0 ? number : 0;
     }
 }
--- typescript/game-of-life/src/Game.ts
+++ typescript/game-of-life/src/Game.ts
@@ -4,6 +4,15 @@ import {Dimensions} from "./Dimensions";
 import {Position} from "./Position";
 import {RuleEngine} from "./RuleEngine";
 
+export class Cell {
+    private _state: CellState;
+    private _position: Position;
+    public constructor(state: CellState, position: Position) {
+        this._state = state;
+        this._position = position;
+    }
+}
+
 export class Game {
     private _dimensions: Dimensions;
     private _board: Board = new Board();
@@ -33,6 +42,14 @@ export class Game {
             });
 
         this._board = newBoard;
+    }
+
+    private getNextStateOfCellAt(position: Position): Cell {
+        const livingNeighbours = this._board.countLivingNeighboursOf(position);
+        const currentState = this._board.isCellAliveAt(position)
+            ? CellState.Living : CellState.Dead;
+        const nextState = RuleEngine.nextState(currentState, livingNeighbours);
+        return new Cell(nextState, position);
     }
 
     private nextStateOf(position: Position) {
