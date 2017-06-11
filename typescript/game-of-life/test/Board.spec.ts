09f0ba3b72c88f1b4eb02af8fb73de32f45d40b6
Christoph Welcz <christoph@welcz.de>
2017-06-11 17:20:11 +0200

    prepare to replace isCellAliveAt function

--- typescript/game-of-life/test/Board.spec.ts
+++ typescript/game-of-life/test/Board.spec.ts
@@ -8,8 +8,8 @@ describe("Board", () => {
     beforeEach(() => target = new Board());
 
     it("should have all cells initially dead", () => {
-        expect(target.isCellAliveAt(new Position(1, 1))).to.be.false;
-        expect(target.isCellAliveAt(new Position(2, 0))).to.be.false;
+        expect(isCellAliveAt(new Position(1, 1))).to.be.false;
+        expect(isCellAliveAt(new Position(2, 0))).to.be.false;
     });
 
     it("should allow cells to be set to alive", () => {
@@ -18,12 +18,12 @@ describe("Board", () => {
             new Position(1, 2),
             new Position(5, 3),
         );
-        expect(target.isCellAliveAt(new Position(1, 2))).to.be.true;
+        expect(isCellAliveAt(new Position(1, 2))).to.be.true;
     });
 
     it("should memorize the position of the cell that is alive", () => {
         target.setCellsAliveAt(new Position(1, 2));
-        expect(target.isCellAliveAt(new Position(1, 1))).to.be.false;
+        expect(isCellAliveAt(new Position(1, 1))).to.be.false;
     });
 
     it("should count living neighbours of cell without neighbours correctly", () => {
@@ -61,6 +61,10 @@ describe("Board", () => {
     it("should let a living cell die", () => {
         target.setCellsAliveAt(new Position(1, 1));
         target.setCellsDeadAt(new Position(1, 1));
-        expect(target.isCellAliveAt(new Position(1, 1))).to.be.false;
+        expect(isCellAliveAt(new Position(1, 1))).to.be.false;
     });
+
+    function isCellAliveAt(position: Position) {
+        return target.isCellAliveAt(position);
+    }
 });
