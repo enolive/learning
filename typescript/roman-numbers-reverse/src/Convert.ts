1f56415fddb55885f4ed6f765b2ad77b8a9dc794
Christoph Welcz <christoph@welcz.de>
2017-06-15 18:16:04 +0200

    introduced constant

--- typescript/roman-numbers-reverse/src/Convert.ts
+++ typescript/roman-numbers-reverse/src/Convert.ts
@@ -4,15 +4,15 @@ import {Rule} from "./Rule";
 export class Convert {
     public static toRomanNumber(arabic: number): string {
         const conversion = new ArabicConversion(arabic);
-        const rules = [
-            new Rule("X", 10),
-            new Rule("IX", 9),
-            new Rule("V", 5),
-            new Rule("IV", 4),
-            new Rule("I", 1),
-        ];
-
-        rules.forEach((r) => conversion.apply(r));
+        Convert.rules.forEach((r) => conversion.apply(r));
         return conversion.result;
     }
+
+    private static rules = [
+        new Rule("X", 10),
+        new Rule("IX", 9),
+        new Rule("V", 5),
+        new Rule("IV", 4),
+        new Rule("I", 1),
+    ];
 }
