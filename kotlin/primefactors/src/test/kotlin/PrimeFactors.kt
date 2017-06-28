eb06b9fc3013e598af4a23d94460dfd6ab04c833
Christoph Welcz <christoph@welcz.de>
2017-06-28 14:41:04 +0200

    some refactoring

--- kotlin/primefactors/src/test/kotlin/PrimeFactors.kt
+++ kotlin/primefactors/src/test/kotlin/PrimeFactors.kt
@@ -1,14 +1,23 @@
 class PrimeFactors {
     fun split(i: Int): Iterable<Int> {
-        val list = ArrayList<Int>()
-        var input = i
-        if (input % 3 == 0) {
-            list.add(3)
+        return PrimeFactors.of(i)
+    }
+
+    companion object {
+        fun of(number: Int): ArrayList<Int> {
+            val list = ArrayList<Int>()
+            val input = number
+            split(input, 2, list)
+            split(input, 3, list)
+            return list
         }
-        while (input % 2 == 0) {
-            list.add(2)
-            input /= 2
+
+        private fun split(remainder: Int, factor: Int, extractedFactors: ArrayList<Int>) {
+            var copy = remainder
+            while (copy % factor == 0) {
+                extractedFactors.add(factor)
+                copy /= factor
+            }
         }
-        return list
     }
 }
--- kotlin/primefactors/src/test/kotlin/PrimeFactorsTest.kt
+++ kotlin/primefactors/src/test/kotlin/PrimeFactorsTest.kt
@@ -6,23 +6,27 @@ class PrimeFactorsTest : FunSpec() {
 
     init {
         test("2 returns 2", {
-            assertThat(factors.split(2)).containsExactly(2)           
+            assertThat(factors.split(2)).containsExactlyInAnyOrder(2)           
         })
         
         test("3 returns 3", {
-            assertThat(factors.split(3)).containsExactly(3)
+            assertThat(factors.split(3)).containsExactlyInAnyOrder(3)
         })
         
         test("4 returns 2,2", {
-            assertThat(factors.split(4)).containsExactly(2, 2)
+            assertThat(factors.split(4)).containsExactlyInAnyOrder(2, 2)
         })
         
         test("6 returns 3,2", {
-            assertThat(factors.split(6)).containsExactly(3, 2)  
+            assertThat(factors.split(6)).containsExactlyInAnyOrder(3, 2)  
         })
         
         test("8 returns 2,2,2", {
-            assertThat(factors.split(8)).containsExactly(2, 2, 2)
+            assertThat(factors.split(8)).containsExactlyInAnyOrder(2, 2, 2)
+        })
+
+        test("9 returns 3,3", {
+            assertThat(factors.split(9)).containsExactlyInAnyOrder(3, 3)
         })
     }
 
