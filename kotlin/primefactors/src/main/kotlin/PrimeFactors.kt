d6ce42a3830127afb66bd462b941533855f993ff
Christoph Welcz <christoph@welcz.de>
2017-06-28 19:17:07 +0200

    forEach is so much nicer ;-)

--- kotlin/primefactors/src/main/kotlin/PrimeFactors.kt
+++ kotlin/primefactors/src/main/kotlin/PrimeFactors.kt
@@ -3,9 +3,7 @@ class PrimeFactors {
     companion object {
         fun of(number: Int): Iterable<Int> {
             val result = Result(number)
-            for (factor in 2..5) {
-                result.split(factor)
-            }
+            (2..5).forEach { factor -> result.split(factor) }
             return result.list
         }
     }
