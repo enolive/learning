044344a23f97006a326629839634122e06def9db
Christoph Welcz <christoph@welcz.de>
2017-06-28 19:37:17 +0200

    FP 2.0

--- kotlin/primefactors/src/main/kotlin/PrimeFactors.kt
+++ kotlin/primefactors/src/main/kotlin/PrimeFactors.kt
@@ -3,7 +3,11 @@ class PrimeFactors {
     companion object {
         fun of(number: Int): Iterable<Int> {
             val result = Result(number)
-            (2..number).forEach { factor -> result.split(factor) }
+            return doItFp(result)
+        }
+
+        private fun doItFp(result: Result): Iterable<Int> {
+            (2..result.input).forEach { factor -> result.split(factor) }
             return result.list
         }
     }
--- kotlin/primefactors/src/main/kotlin/Result.kt
+++ kotlin/primefactors/src/main/kotlin/Result.kt
@@ -1,4 +1,4 @@
-class Result(input: Int) {
+class Result(val input: Int) {
     private var remainder = input
     private val _list = ArrayList<Int>()
 
