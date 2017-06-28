b5dc11ece795b43ebcbe83002253f6c88680a997
Christoph Welcz <christoph@welcz.de>
2017-06-28 19:13:05 +0200

    split into separate files

--- kotlin/primefactors/src/test/kotlin/PrimeFactors.kt
+++ kotlin/primefactors/src/test/kotlin/PrimeFactors.kt
@@ -3,36 +3,10 @@ class PrimeFactors {
     companion object {
         fun of(number: Int): Iterable<Int> {
             val result = Result(number)
-            for (i in arrayOf(2, 3, 5)) {
-                result.split(i)
+            for (factor in arrayOf(2, 3, 5)) {
+                result.split(factor)
             }
             return result.list
         }
     }
-}
-
-class Result(input: Int) {
-    private var remainder = input
-    private val _list = ArrayList<Int>()
-
-    val list: Iterable<Int>
-        get() = _list
-
-    fun split(factor: Int) {
-        while (canBeFactorized(factor)) {
-            addToList(factor)
-            removeFromNumber(factor)
-        }
-    }
-
-    private fun removeFromNumber(factor: Int) {
-        remainder /= factor
-    }
-
-    private fun addToList(factor: Int) {
-        _list.add(factor)
-    }
-
-    private fun canBeFactorized(factor: Int) = remainder % factor == 0
-
 }
---
+++ kotlin/primefactors/src/test/kotlin/Result.kt
@@ -0,0 +1,25 @@
+class Result(input: Int) {
+    private var remainder = input
+    private val _list = ArrayList<Int>()
+
+    val list: Iterable<Int>
+        get() = _list
+
+    fun split(factor: Int) {
+        while (canBeFactorized(factor)) {
+            addToList(factor)
+            removeFromNumber(factor)
+        }
+    }
+
+    private fun removeFromNumber(factor: Int) {
+        remainder /= factor
+    }
+
+    private fun addToList(factor: Int) {
+        _list.add(factor)
+    }
+
+    private fun canBeFactorized(factor: Int) = remainder % factor == 0
+
+}
