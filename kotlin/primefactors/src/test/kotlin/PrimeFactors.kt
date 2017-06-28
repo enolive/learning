057eac8acfa0159f1dbed8937fbbb4b632760094
Christoph Welcz <christoph@welcz.de>
2017-06-28 13:46:50 +0200

    can split 2s now!

--- kotlin/primefactors/src/test/kotlin/PrimeFactors.kt
+++ kotlin/primefactors/src/test/kotlin/PrimeFactors.kt
@@ -1,5 +1,11 @@
 class PrimeFactors {
-    fun split(): Array<Int> {
-        return arrayOf(2)
+    fun split(i: Int): Iterable<Int> {
+        val list = ArrayList<Int>()
+        var input = i
+        while (input % 2 == 0) {
+            list.add(2)
+            input /= 2
+        }
+        return list
     }
 }
--- kotlin/primefactors/src/test/kotlin/PrimeFactorsTest.kt
+++ kotlin/primefactors/src/test/kotlin/PrimeFactorsTest.kt
@@ -2,11 +2,19 @@ import io.kotlintest.specs.FunSpec
 import org.assertj.core.api.Assertions.assertThat
 
 class PrimeFactorsTest : FunSpec() {
+    var factors = PrimeFactors()
+
     init {
-        var factors = PrimeFactors().split()
-        
         test("2 returns 2", {
-            assertThat(factors).containsExactly(2)           
+            assertThat(factors.split(2)).containsExactly(2)           
+        })
+        
+        test("4 returns 2,2", {
+            assertThat(factors.split(4)).containsExactly(2, 2)
+        })
+        
+        test("8 returns 2,2,2", {
+            assertThat(factors.split(8)).containsExactly(2, 2, 2)
         })
     }
 
