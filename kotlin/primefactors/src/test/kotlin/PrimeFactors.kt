e4156e99cf595b8e11e4267ff7e685a2498675ca
Christoph Welcz <christoph@welcz.de>
2017-06-28 19:05:32 +0200

    removing feature envy

--- kotlin/primefactors/src/test/kotlin/PrimeFactors.kt
+++ kotlin/primefactors/src/test/kotlin/PrimeFactors.kt
@@ -5,7 +5,7 @@ class PrimeFactors {
             val input = number
             val result = Result(input)
             for (i in arrayOf(2, 3, 5)) {
-                split(result, i)
+                result.split(i)
             }
             return result.list
         }
@@ -33,5 +33,23 @@ class PrimeFactors {
 class Result(input: Int) {
     var remainder = input
     val list = ArrayList<Int>()
+    val result = this
+
+    fun split(factor: Int) {
+        while (canBeFactorized(factor)) {
+            add(factor)
+            remove(factor)
+        }
+    }
+
+    private fun remove(factor: Int) {
+        result.remainder /= factor
+    }
+
+    private fun add(factor: Int) {
+        result.list.add(factor)
+    }
+
+    private fun canBeFactorized(factor: Int) = result.remainder % factor == 0
 
 }
