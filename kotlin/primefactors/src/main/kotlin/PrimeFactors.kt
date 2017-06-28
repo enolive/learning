cf47ce358a81446e8a1595e523a4174cb83fbf19
Christoph Welcz <christoph@welcz.de>
2017-06-28 19:16:03 +0200

    work with a range

--- kotlin/primefactors/src/main/kotlin/PrimeFactors.kt
+++ kotlin/primefactors/src/main/kotlin/PrimeFactors.kt
@@ -3,7 +3,7 @@ class PrimeFactors {
     companion object {
         fun of(number: Int): Iterable<Int> {
             val result = Result(number)
-            for (factor in arrayOf(2, 3, 4, 5)) {
+            for (factor in 2..5) {
                 result.split(factor)
             }
             return result.list
