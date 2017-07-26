bc776321933d2f454d7e72af71461542add2f7c4
Christoph Welcz <christoph@welcz.de>
2017-07-26 20:10:54 +0200

    Initial working setup for fizz-buzz kata

---
+++ java/demo-kata/src/test/java/FizzBuzzTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FizzBuzzTest {
+    @Test
+    public void testSomething() {
+        assertThat(true).isFalse();
+    }
+}
