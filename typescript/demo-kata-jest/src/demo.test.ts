34f228f5cd0dacb1b97bc967d952c5e40a4127c5
Christoph Welcz <christoph@welcz.de>
2017-08-18 19:04:06 +0200

    some small improvements

--- typescript/demo-kata-jest/src/demo.test.ts
+++ typescript/demo-kata-jest/src/demo.test.ts
@@ -1,16 +1,5 @@
-class Greeter {
-    public sayHello() {
-        return "Hello, World!";
-    }
-}
-
 describe("Some test", () => {
     it("should succeed", () => {
-        expect(true).toBeFalsy();
-    });
-
-    it("should greet the world", () => {
-        const greeter = new Greeter();
-        expect(greeter.sayHello()).toEqual("Hello, World!");
-    });
-});
+        expect(true).toBeFalsy()
+    })
+})
