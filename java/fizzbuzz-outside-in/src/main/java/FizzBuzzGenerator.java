0084826ad3caeaab9dfca45a227338c884e89bbb
Christoph Welcz <christoph@welcz.de>
2017-08-22 23:35:20 +0200

    fake it until you make it

---
+++ java/fizzbuzz-outside-in/src/main/java/FizzBuzzGenerator.java
@@ -0,0 +1,5 @@
+public class FizzBuzzGenerator {
+    public String calculate(int input) {
+        return "1";
+    }
+}
--- java/fizzbuzz-outside-in/src/test/java/FizzBuzzTest.java
+++ java/fizzbuzz-outside-in/src/test/java/FizzBuzzTest.java
@@ -4,7 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzzTest {
     @Test
-    public void doSomething() {
-        assertThat(true).isFalse();
+    public void calculateShouldReturn1For1() {
+        FizzBuzzGenerator generator = new FizzBuzzGenerator();
+        String result = generator.calculate(1);
+        assertThat(result).isEqualTo("1");
     }
 }
