236efc384bb4588056beafca1452ca000807dd5c
Christoph Welcz <christoph@welcz.de>
2017-08-25 17:36:57 +0200

    basic setup

--- java/fizzbuzz-outside-in/src/test/java/FizzBuzzTest.java
+++ java/fizzbuzz-outside-in/src/test/java/FizzBuzzTest.java
@@ -1,12 +1,21 @@
-import org.junit.Test;
+import com.mscharhag.oleaster.runner.OleasterRunner;
+import org.junit.runner.RunWith;
 
+import static com.mscharhag.oleaster.runner.StaticRunnerSupport.describe;
+import static com.mscharhag.oleaster.runner.StaticRunnerSupport.it;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(OleasterRunner.class)
 public class FizzBuzzTest {
-
-    @Test
-    public void calculateShouldReturn1For1() {
-        FizzBuzzGenerator generator = new FizzBuzzGenerator();
-        assertThat(generator.calculate(1)).isEqualTo("1");
+    static {
+        describe("Fizz Buzz Generator", () -> {
+            describe("Outside-in", () -> {
+                it("should do something", () -> {});
+            });
+            it("should return 1 for 1", () -> {
+                FizzBuzzGenerator generator = new FizzBuzzGenerator();
+                assertThat(generator.calculate(1)).isEqualTo("1");
+            });
+        });
     }
 }
