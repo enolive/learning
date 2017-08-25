49018f4978a9f442129ba6a3d9377ea1060679b2
Christoph Welcz <christoph@welcz.de>
2017-08-25 17:44:32 +0200

    classic approach

--- java/fizzbuzz-outside-in/src/main/java/FizzBuzzGenerator.java
+++ java/fizzbuzz-outside-in/src/main/java/FizzBuzzGenerator.java
@@ -1,5 +1,21 @@
 public class FizzBuzzGenerator {
     public String calculate(int input) {
-        return "1";
+        if (isDivisibleBy(input, 3) && isDivisibleBy(input, 5)) {
+            return "Buzz";
+        }
+
+        if (isDivisibleBy(input, 5)) {
+            return "Buzz";
+        }
+        
+        if (isDivisibleBy(input, 3)) {
+            return "Fizz";
+        }
+        
+        return String.valueOf(input);
+    }
+
+    private boolean isDivisibleBy(int input, int denominator) {
+        return input % denominator == 0;
     }
 }
--- java/fizzbuzz-outside-in/src/test/java/FizzBuzzTest.java
+++ java/fizzbuzz-outside-in/src/test/java/FizzBuzzTest.java
@@ -9,12 +9,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class FizzBuzzTest {
     static {
         describe("Fizz Buzz Generator", () -> {
-            describe("Outside-in", () -> {
-                it("should do something", () -> {});
-            });
-            it("should return 1 for 1", () -> {
+            it("should return normal numbers as is", () -> {
                 FizzBuzzGenerator generator = new FizzBuzzGenerator();
                 assertThat(generator.calculate(1)).isEqualTo("1");
+                assertThat(generator.calculate(2)).isEqualTo("2");
+            });
+
+            it("should return Fizz for numbers divisible by 3", () -> {
+                FizzBuzzGenerator generator = new FizzBuzzGenerator();
+                assertThat(generator.calculate(3)).isEqualTo("Fizz");
+                assertThat(generator.calculate(6)).isEqualTo("Fizz");
+            });
+
+            it("should return Buzz for numbers divisible by 5", () -> {
+                FizzBuzzGenerator generator = new FizzBuzzGenerator();
+                assertThat(generator.calculate(5)).isEqualTo("Buzz");
+                assertThat(generator.calculate(10)).isEqualTo("Buzz");
+            });
+
+            it("should return Fizz-Buzz for numbers divisible by 3 and 5", () -> {
+                FizzBuzzGenerator generator = new FizzBuzzGenerator();
+                assertThat(generator.calculate(15)).isEqualTo("Buzz");
+                assertThat(generator.calculate(30)).isEqualTo("Buzz");
             });
         });
     }
