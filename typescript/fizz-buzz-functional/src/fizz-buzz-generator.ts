db7dcab2229f4b96654f32dec265ca1d6f474c29
Christoph Welcz <christoph@welcz.de>
2017-09-04 10:44:25 +0200

    test for fizz

--- typescript/fizz-buzz-functional/src/fizz-buzz-generator.ts
+++ typescript/fizz-buzz-functional/src/fizz-buzz-generator.ts
@@ -1,5 +1,9 @@
 export class FizzBuzzGenerator {
     generate(input: number) {
+        if (input % 3 === 0) {
+            return 'Fizz'
+        }
+
         return input.toString()
     }
 }
--- typescript/fizz-buzz-functional/test/fizz-buzz-generator.spec.ts
+++ typescript/fizz-buzz-functional/test/fizz-buzz-generator.spec.ts
@@ -1,13 +1,18 @@
-import {expect} from "chai"
-import {FizzBuzzGenerator} from "../src/fizz-buzz-generator"
+import {expect} from 'chai'
+import {FizzBuzzGenerator} from '../src/fizz-buzz-generator'
 
-describe("Fizz-Buzz Generator", () => {
+describe('Fizz-Buzz Generator', () => {
     let generator: FizzBuzzGenerator
 
     beforeEach(() => generator = new FizzBuzzGenerator())
 
-    it("should return normal numbers as is", () => {
-        expect(generator.generate(1)).to.equal("1")
-        expect(generator.generate(2)).to.equal("2")
+    it('should return normal numbers as is', () => {
+        expect(generator.generate(1)).to.equal('1')
+        expect(generator.generate(2)).to.equal('2')
+    })
+
+    it('should return Fizz for numbers divisible by 3', () => {
+        expect(generator.generate(3)).to.equal('Fizz')
+        expect(generator.generate(6)).to.equal('Fizz')
     })
 })
