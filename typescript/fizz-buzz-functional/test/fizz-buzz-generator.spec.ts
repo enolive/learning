9ba4b6d506c5442083abd62f54d19beb855daa54
Christoph Welcz <christoph@welcz.de>
2017-09-04 10:42:05 +0200

    moved SUT creation to before eacch

--- typescript/fizz-buzz-functional/test/fizz-buzz-generator.spec.ts
+++ typescript/fizz-buzz-functional/test/fizz-buzz-generator.spec.ts
@@ -1,10 +1,13 @@
 import {expect} from "chai"
 import {FizzBuzzGenerator} from "../src/fizz-buzz-generator"
 
-describe('Fizz-Buzz Generator', () => {
-    it('should return normal numbers as is', () => {
-        const generator = new FizzBuzzGenerator()
-        expect(generator.generate(1)).to.equal('1')
-        expect(generator.generate(2)).to.equal('2')
+describe("Fizz-Buzz Generator", () => {
+    let generator: FizzBuzzGenerator
+
+    beforeEach(() => generator = new FizzBuzzGenerator())
+
+    it("should return normal numbers as is", () => {
+        expect(generator.generate(1)).to.equal("1")
+        expect(generator.generate(2)).to.equal("2")
     })
 })
