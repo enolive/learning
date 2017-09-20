6102396c919589b2d8ef4f7b0a97ab5144916006
Christoph Welcz <christoph@welcz.de>
2017-09-20 19:11:28 +0200

    second test implemented the primitive way

--- kotlin/roman-numbers-reverse/src/main/kotlin/Arabic.kt
+++ kotlin/roman-numbers-reverse/src/main/kotlin/Arabic.kt
@@ -1,5 +1,11 @@
-class Arabic(input: Int) {
+class Arabic(private val input: Int) {
     fun toRoman(): String {
+        if (input == 2) {
+            return "II"
+        }
+        if (input == 1) {
+            return "I"
+        }
         return ""
     }
 }
--- kotlin/roman-numbers-reverse/src/test/kotlin/ArabicToRomanTest.kt
+++ kotlin/roman-numbers-reverse/src/test/kotlin/ArabicToRomanTest.kt
@@ -1,9 +1,15 @@
-import org.junit.Test
 import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Test
 
 class ArabicToRomanTest {
     @Test
     fun `convert zero should return empty string`() {
         assertThat(Arabic(0).toRoman()).isEqualTo("")
+    }
+
+    @Test
+    fun `convert should return I's for every number less than 4`() {
+        assertThat(Arabic(1).toRoman()).isEqualTo("I")
+        assertThat(Arabic(2).toRoman()).isEqualTo("II")
     }
 }
