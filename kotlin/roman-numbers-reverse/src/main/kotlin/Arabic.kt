9eabc1014a219527d1e93fccc4c4971357cdd25d
Christoph Welcz <christoph@welcz.de>
2017-09-20 21:19:19 +0200

    Revert "implemented all addition tests"
    This reverts commit f655510

--- kotlin/roman-numbers-reverse/src/main/kotlin/Arabic.kt
+++ kotlin/roman-numbers-reverse/src/main/kotlin/Arabic.kt
@@ -1,13 +1,11 @@
 class Arabic(private val input: Int) {
-    private val rules = listOf(
-            Rule(10, "X"),
-            Rule(5, "V"),
-            Rule(1, "I")
-    )
-
-    fun toRoman(): String = rules
-            .fold(Conversion(input)) { conversion, rule -> conversion.apply(rule) }
-            .result
-
+    fun toRoman(): String {
+        if (input == 2) {
+            return "II"
+        }
+        if (input == 1) {
+            return "I"
+        }
+        return ""
+    }
 }
-
