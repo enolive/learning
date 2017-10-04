6188eb4c694886ce34889e05651135dfab6471bc
Christoph Welcz <christoph@welcz.de>
2017-10-04 20:14:51 +0200

    tried out DisplayName as an alternative to weird test file names

--- kotlin/demo-kata/src/test/kotlin/DemoTest.kt
+++ kotlin/demo-kata/src/test/kotlin/DemoTest.kt
@@ -1,15 +1,18 @@
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
+@DisplayName("Better name")
 class DemoTest {
     @Test
     fun `framework should work`() {
         assertThat(true).isFalse()
     }
-    
+
     @Nested
-    inner class `Nested tests work as well` {
+    @DisplayName("Nested tests work as well")
+    inner class NestedTests {
         @Test
         fun `this should also work`() {
             assertThat(false).isTrue()
