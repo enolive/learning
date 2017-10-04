267210777f318ccea70b247a08be98c638ed8e73
Christoph Welcz <christoph@welcz.de>
2017-10-04 19:52:16 +0200

    Demo Kata in Kotlin with working gradle configuration for running all tests

---
+++ kotlin/demo-kata/src/test/kotlin/Demo for Jupiter Tests in Kotlin.kt
@@ -0,0 +1,9 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class `Demo for Jupiter Tests in Kotlin` {
+    @Test
+    fun `framework should work`() {
+        assertThat(true).isFalse()
+    }
+}
