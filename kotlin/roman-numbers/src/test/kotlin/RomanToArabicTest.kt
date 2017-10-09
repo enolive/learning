2e156b5fd9bdfd8b3ed4df52672a0f4db3695dde
Christoph Welcz <christoph@welcz.de>
2017-10-09 21:39:37 +0200

    second test

--- kotlin/roman-numbers/src/test/kotlin/RomanToArabicTest.kt
+++ kotlin/roman-numbers/src/test/kotlin/RomanToArabicTest.kt
@@ -5,11 +5,15 @@ class RomanToArabicTest {
     @Test
     fun `return I's as 1`() {
         assertThat(Roman("I").toArabic()).isEqualTo(1)
+        assertThat(Roman("II").toArabic()).isEqualTo(2)
     }
 }
 
-class Roman(input: String) {
+class Roman(val input: String) {
     fun toArabic(): Int {
+        if (input == "II") {
+            return 2
+        }
         return 1
     }
 
