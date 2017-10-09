652cf68d06f3371579a34e97d40ad871e964add7
Christoph Welcz <christoph@welcz.de>
2017-10-09 22:52:53 +0200

    removed the bug by cutting the length of the roman digit instead will leave the guard clause, it will reveal missing or invalid digits

--- kotlin/roman-numbers/src/main/kotlin/Conversion.kt
+++ kotlin/roman-numbers/src/main/kotlin/Conversion.kt
@@ -1,16 +1,16 @@
-internal class Conversion(val remainingInput: String, val sum: Int = 0) {
+internal class Conversion(private val remainingInput: String, val sum: Int = 0) {
     fun apply(rule: Rule): Conversion {
         tailrec fun applyRec(remainingInput: String, sum: Int): Conversion {
             return when {
                 !remainingInput.startsWith(rule.roman) -> Conversion(remainingInput, sum)
-                else -> applyRec(remainingInput.cutAtStart(), sum + rule.arabic)
+                else -> applyRec(remainingInput.cutAtStart(rule.roman), sum + rule.arabic)
             }
         }
         return applyRec(remainingInput, sum)
     }
 
-    private fun String.cutAtStart(): String = when {
-        !this.isEmpty() -> this.substring(1)
+    private fun String.cutAtStart(roman: String): String = when {
+        this.length >= roman.length -> this.substring(roman.length)
         else -> this
     }
 
