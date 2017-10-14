e2502c00d18cd6d6763fc7b89be101f37608b8ba
Christoph Welcz <christoph@welcz.de>
2017-10-14 07:22:51 +0200

    initial setup

---
+++ java/rock-paper-scissors/src/test/java/RockPaperScissorsTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RockPaperScissorsTest {
+    @Test
+    public void frameworkShouldWork() {
+        assertThat(true).isTrue();
+    }
+}
