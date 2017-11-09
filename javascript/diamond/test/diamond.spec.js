714b60d701a91b6fccc27baa7cfc80272fb2e5de
Christoph Welcz <christoph@welcz.de>
2017-11-09 16:40:38 +0100

    initial setup

---
+++ javascript/diamond/test/diamond.spec.js
@@ -0,0 +1,10 @@
+'use strict'
+
+import {expect} from 'chai'
+import {describe, it} from 'mocha'
+
+describe('Tests with Mocha', () => {
+    it('should work', () => {
+        expect(true).to.be.equal(false)
+    })
+})
