f82742dd543b9ca51c381dc605dbbd445872d73a
Christoph Welcz <christoph@welcz.de>
2017-11-10 17:10:12 +0100

    removed feature envy

--- kotlin/bowling/src/main/kotlin/Bowling.kt
+++ kotlin/bowling/src/main/kotlin/Bowling.kt
@@ -10,46 +10,10 @@ class Bowling {
             var current = CurrentScore(0, 0, rolls)
 
             allFrames().forEach {
-                current = next(current)
+                current = current.next()
             }
             return current.points
         }
-
-    private fun next(current: CurrentScore): CurrentScore {
-        return when {
-            isStrike(current) -> scoreStrike(current)
-            isSpare(current) -> scoreSpare(current)
-            else -> scoreNormalFrame(current)
-        }
-    }
-
-    private fun isStrike(current: CurrentScore) = current.rolls[current.rollIndex] == 10
-
-    private fun scoreStrike(current: CurrentScore): CurrentScore {
-        val nextScore = current.points + strikePoints(current)
-        return CurrentScore(nextScore, current.rollIndex + 1, current.rolls)
-    }
-
-    private fun strikePoints(current: CurrentScore) =
-            (current.rolls[current.rollIndex + 1] + current.rolls[current.rollIndex + 2] + 10)
-
-    private fun isSpare(current: CurrentScore) =
-            current.rolls[current.rollIndex] + current.rolls[current.rollIndex + 1] == 10
-
-    private fun scoreNormalFrame(current: CurrentScore): CurrentScore {
-        val nextScore = current.points + normalPoints(current)
-        return CurrentScore(nextScore, current.rollIndex + 2, current.rolls)
-    }
-
-    private fun scoreSpare(current: CurrentScore): CurrentScore {
-        val nextScore = current.points + sparePoints(current)
-        return CurrentScore(nextScore, current.rollIndex + 2, current.rolls)
-    }
-
-    private fun normalPoints(current: CurrentScore) =
-            current.rolls[current.rollIndex] + current.rolls[current.rollIndex + 1]
-
-    private fun sparePoints(current: CurrentScore) = 10 + current.rolls[current.rollIndex + 2]
 
     private fun allFrames() = (1..10)
 
--- kotlin/bowling/src/main/kotlin/CurrentScore.kt
+++ kotlin/bowling/src/main/kotlin/CurrentScore.kt
@@ -1,3 +1,39 @@
 class CurrentScore(val points: Int, val rollIndex: Int, val rolls: List<Int>) {
+    fun next(): CurrentScore {
+        return when {
+            isStrike() -> scoreStrike()
+            isSpare() -> scoreSpare()
+            else -> scoreNormalFrame()
+        }
+    }
+
+    private fun isStrike() = rolls[rollIndex] == 10
+
+    private fun scoreStrike(): CurrentScore {
+        val nextScore = points + strikePoints()
+        return CurrentScore(nextScore, rollIndex + 1, rolls)
+    }
+
+    private fun strikePoints() =
+            (rolls[rollIndex + 1] + rolls[rollIndex + 2] + 10)
+
+    private fun isSpare() =
+            rolls[rollIndex] + rolls[rollIndex + 1] == 10
+
+    private fun scoreNormalFrame(): CurrentScore {
+        val nextScore = points + normalPoints()
+        return CurrentScore(nextScore, rollIndex + 2, rolls)
+    }
+
+    private fun scoreSpare(): CurrentScore {
+        val nextScore = points + sparePoints()
+        return CurrentScore(nextScore, rollIndex + 2, rolls)
+    }
+
+    private fun normalPoints() =
+            rolls[rollIndex] + rolls[rollIndex + 1]
+
+    private fun sparePoints() = 10 + rolls[rollIndex + 2]
+
 
 }
