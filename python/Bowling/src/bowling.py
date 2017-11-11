0c9e145636081ab1b160755764f3d41250328df1
Christoph Welcz <christoph@welcz.de>
2017-11-11 19:02:26 +0100

    some refactorings

--- python/Bowling/src/bowling.py
+++ python/Bowling/src/bowling.py
@@ -10,8 +10,7 @@ class Bowling(object):
         ball_index = 0
         for frame in range(0, 10):
             if self.rolls[ball_index] == 10:
-                score += self.score_strike(ball_index)
-                ball_index += 1
+                ball_index, score = self.score_strike(ball_index, score)
             elif self.score_frame(ball_index) == 10:
                 score += self.score_spare(ball_index)
                 ball_index += 2
@@ -20,8 +19,10 @@ class Bowling(object):
                 ball_index += 2
         return score
 
-    def score_strike(self, ball_index: int) -> int:
-        return 10 + self.rolls[ball_index + 1] + self.rolls[ball_index + 2]
+    def score_strike(self, ball_index, score):
+        score += 10 + self.rolls[ball_index + 1] + self.rolls[ball_index + 2]
+        ball_index += 1
+        return ball_index, score
 
     def score_frame(self, ball_index: int) -> int:
         return self.rolls[ball_index] + self.rolls[ball_index + 1]
