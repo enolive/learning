a95a52c2dcf58369de9ace9d8e23e02a7f12c6fe
Christoph Welcz <christoph@welcz.de>
2017-11-11 19:33:21 +0100

    some currying

--- python/Bowling/src/bowling.py
+++ python/Bowling/src/bowling.py
@@ -13,16 +13,17 @@ class Bowling(object):
         return score
 
     def score_frame(self, ball_index: int, score: int) -> (int, int):
-        ball_index, score = self.get_scoring_function(ball_index)(ball_index, score)
+        ball_index, score = self.get_scoring_function(ball_index)(ball_index)(score)
         return score, ball_index
 
     def get_scoring_function(self, ball_index):
         if self.is_strike(ball_index):
-            return self.score_strike
+            fun = self.score_strike
         elif self.is_spare(ball_index):
-            return self.score_spare
+            fun = self.score_spare
         else:
-            return self.score_normal_frame
+            fun = self.score_normal_frame
+        return fun
 
     def is_strike(self, ball_index: int) -> bool:
         return self.rolls[ball_index] == 10
@@ -30,14 +31,20 @@ class Bowling(object):
     def is_spare(self, ball_index: int) -> bool:
         return self.get_frame_score(ball_index) == 10
 
-    def score_strike(self, ball_index: int, score: int) -> (int, int):
-        return ball_index + 1, score + 10 + self.get_frame_score(ball_index + 1)
+    def score_strike(self, ball_index: int) -> (int, int):
+        def g(score):
+            return ball_index + 1, score + 10 + self.get_frame_score(ball_index + 1)
+        return g
 
-    def score_spare(self, ball_index: int, score: int) -> (int, int):
-        return ball_index + 2, score + 10 + self.rolls[ball_index + 2]
+    def score_spare(self, ball_index: int) -> (int, int):
+        def g(score):
+            return ball_index + 2, score + 10 + self.rolls[ball_index + 2]
+        return g
 
-    def score_normal_frame(self, ball_index: int, score: int) -> (int, int):
-        return ball_index + 2, score + self.get_frame_score(ball_index)
+    def score_normal_frame(self, ball_index: int) -> (int, int):
+        def g(score):
+            return ball_index + 2, score + self.get_frame_score(ball_index)
+        return g
 
     def get_frame_score(self, ball_index: int) -> int:
         return self.rolls[ball_index] + self.rolls[ball_index + 1]
