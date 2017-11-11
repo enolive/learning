55c847d524aeec97239c2b3d2a2cde9fca87bd6a
Christoph Welcz <christoph@welcz.de>
2017-11-11 19:17:07 +0100

    easified some methods

--- python/Bowling/src/bowling.py
+++ python/Bowling/src/bowling.py
@@ -5,13 +5,14 @@ class Bowling(object):
     def roll(self, pins: int) -> None:
         self.rolls.append(pins)
 
+    @property
     def get_score(self) -> int:
         score, ball_index = 0, 0
         for frame in range(0, 10):
             score, ball_index = self.score_frame(ball_index, score)
         return score
 
-    def score_frame(self, ball_index, score):
+    def score_frame(self, ball_index: int, score: int) -> (int, int):
         if self.is_strike(ball_index):
             ball_index, score = self.score_strike(ball_index, score)
         elif self.is_spare(ball_index):
@@ -20,23 +21,21 @@ class Bowling(object):
             ball_index, score = self.score_normal_frame(ball_index, score)
         return score, ball_index
 
-    def is_strike(self, ball_index):
+    def is_strike(self, ball_index: int) -> bool:
         return self.rolls[ball_index] == 10
 
-    def is_spare(self, ball_index):
+    def is_spare(self, ball_index: int) -> bool:
         return self.get_frame_score(ball_index) == 10
 
-    def score_normal_frame(self, ball_index, score):
-        score += self.rolls[ball_index] + self.rolls[ball_index + 1]
-        ball_index += 2
-        return ball_index, score
+    def score_normal_frame(self, ball_index: int, score: int) -> (int, int):
+        return ball_index + 2, score + self.get_frame_score(ball_index)
 
-    def score_spare(self, ball_index, score):
+    def score_spare(self, ball_index: int, score: int) -> (int, int):
         score += 10 + self.rolls[ball_index + 2]
         ball_index += 2
         return ball_index, score
 
-    def score_strike(self, ball_index, score):
+    def score_strike(self, ball_index: int, score: int) -> (int, int):
         score += 10 + self.get_frame_score(ball_index + 1)
         ball_index += 1
         return ball_index, score
--- python/Bowling/tests/bowling_tests.py
+++ python/Bowling/tests/bowling_tests.py
@@ -11,22 +11,22 @@ class BowlingTests(unittest.TestCase):
 
     def test_gutter_game(self):
         self.roll_many(0, 20)
-        assert_that(self.bowling.get_score()).is_equal_to(0)
+        assert_that(self.bowling.get_score).is_equal_to(0)
 
     def test_one_point_game(self):
         self.roll_many(1, 20)
-        assert_that(self.bowling.get_score()).is_equal_to(20)
+        assert_that(self.bowling.get_score).is_equal_to(20)
 
     def test_spare(self):
         self.roll_pins(5, 5, 7)
         self.roll_many(0, 17)
-        assert_that(self.bowling.get_score()).is_equal_to(24)
+        assert_that(self.bowling.get_score).is_equal_to(24)
 
     def test_strike(self):
         self.roll_pins(10)
         self.roll_pins(4, 5)
         self.roll_many(0, 16)
-        assert_that(self.bowling.get_score()).is_equal_to(28)
+        assert_that(self.bowling.get_score).is_equal_to(28)
 
     def roll_pins(self, *pins: int) -> None:
         for p in pins:
