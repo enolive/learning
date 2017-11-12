026c2b505ef6eaddadc7cae681549bee5a44e0c9
Christoph Welcz <christoph@welcz.de>
2017-11-12 19:38:32 +0100

    made everything super-private

--- python/Bowling/src/bowling.py
+++ python/Bowling/src/bowling.py
@@ -4,56 +4,56 @@ from typing import Callable, Tuple
 
 class Bowling(object):
     def __init__(self):
-        self.rolls = []
+        self.__rolls = []
 
     def roll(self, pins: int) -> None:
-        self.rolls.append(pins)
+        self.__rolls.append(pins)
 
     @property
-    def get_score(self) -> int:
+    def score(self) -> int:
         _, score = reduce(
-            lambda acc, _: self.score_frame(acc),
-            self.all_frames(),
+            lambda acc, _: self.__score_frame(acc),
+            self.__all_frames(),
             (0, 0)
         )
         return score
 
     @staticmethod
-    def all_frames():
+    def __all_frames():
         return range(0, 10)
 
-    def score_frame(self, current_frame: Tuple[int, int]) -> (int, int):
+    def __score_frame(self, current_frame: Tuple[int, int]) -> (int, int):
         ball_index, score = current_frame
-        return self.get_scoring_function(ball_index)(score)
+        return self.__get_scoring_function(ball_index)(score)
 
-    def get_scoring_function(self, ball_index: int) -> Callable[[int], Tuple[int, int]]:
+    def __get_scoring_function(self, ball_index: int) -> Callable[[int], Tuple[int, int]]:
         rules = (
-            (self.is_strike, self.score_strike),
-            (self.is_spare, self.score_spare),
-            (lambda _: True, self.score_normal_frame),
+            (self.__is_strike, self.__score_strike),
+            (self.__is_spare, self.__score_spare),
+            (lambda _: True, self.__score_normal_frame),
         )
         found_rule = next(map(lambda r: r[1], filter(lambda r: r[0](ball_index), rules)))
         return found_rule(ball_index)
 
-    def is_strike(self, ball_index: int) -> bool:
-        return self.get_ball_points(ball_index) == 10
+    def __is_strike(self, ball_index: int) -> bool:
+        return self.__get_ball_points(ball_index) == 10
 
-    def is_spare(self, ball_index: int) -> bool:
-        return self.get_frame_score(ball_index) == 10
+    def __is_spare(self, ball_index: int) -> bool:
+        return self.__get_frame_score(ball_index) == 10
 
-    def score_strike(self, ball_index: int) -> Callable[[int], Tuple[int, int]]:
+    def __score_strike(self, ball_index: int) -> Callable[[int], Tuple[int, int]]:
         return lambda score: (ball_index + 1, score + 10 +
-                              self.get_ball_points(ball_index + 1) +
-                              self.get_ball_points(ball_index + 2))
+                              self.__get_ball_points(ball_index + 1) +
+                              self.__get_ball_points(ball_index + 2))
 
-    def score_spare(self, ball_index: int) -> Callable[[int], Tuple[int, int]]:
-        return lambda score: (ball_index + 2, score + 10 + self.get_ball_points(ball_index + 2))
+    def __score_spare(self, ball_index: int) -> Callable[[int], Tuple[int, int]]:
+        return lambda score: (ball_index + 2, score + 10 + self.__get_ball_points(ball_index + 2))
 
-    def score_normal_frame(self, ball_index: int) -> Callable[[int], Tuple[int, int]]:
-        return lambda score: (ball_index + 2, score + self.get_frame_score(ball_index))
+    def __score_normal_frame(self, ball_index: int) -> Callable[[int], Tuple[int, int]]:
+        return lambda score: (ball_index + 2, score + self.__get_frame_score(ball_index))
 
-    def get_frame_score(self, ball_index: int) -> int:
-        return self.get_ball_points(ball_index) + self.get_ball_points(ball_index + 1)
+    def __get_frame_score(self, ball_index: int) -> int:
+        return self.__get_ball_points(ball_index) + self.__get_ball_points(ball_index + 1)
 
-    def get_ball_points(self, ball_index):
-        return self.rolls[ball_index] if len(self.rolls) > ball_index else 0
+    def __get_ball_points(self, ball_index):
+        return self.__rolls[ball_index] if len(self.__rolls) > ball_index else 0
--- python/Bowling/tests/bowling_tests.py
+++ python/Bowling/tests/bowling_tests.py
@@ -11,30 +11,30 @@ class BowlingTests(unittest.TestCase):
 
     def test_gutter_game(self):
         self.roll_many(0, 20)
-        assert_that(self.get_score).is_equal_to(0)
+        assert_that(self.score).is_equal_to(0)
 
     def test_one_point_game(self):
         self.roll_many(1, 20)
-        assert_that(self.get_score).is_equal_to(20)
+        assert_that(self.score).is_equal_to(20)
 
     def test_spare(self):
         self.roll_pins(5, 5, 7)
         self.roll_many(0, 17)
-        assert_that(self.get_score).is_equal_to(24)
+        assert_that(self.score).is_equal_to(24)
 
     def test_strike(self):
         self.roll_pins(10)
         self.roll_pins(4, 5)
         self.roll_many(0, 16)
-        assert_that(self.get_score).is_equal_to(28)
+        assert_that(self.score).is_equal_to(28)
 
     def test_perfect_game(self):
         self.roll_many(10, 12)
-        assert_that(self.get_score).is_equal_to(300)
+        assert_that(self.score).is_equal_to(300)
 
     @property
-    def get_score(self):
-        return self.bowling.get_score
+    def score(self):
+        return self.bowling.score
 
     def roll_pins(self, *pins: int) -> None:
         for p in pins:
