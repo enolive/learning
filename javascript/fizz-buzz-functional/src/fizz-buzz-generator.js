476d55f7790c4567483127ac4afbcd3ef1f4184b
Christoph Welcz <christoph@welcz.de>
2017-11-19 11:53:26 +0100

    removed loop

--- javascript/fizz-buzz-functional/src/fizz-buzz-generator.js
+++ javascript/fizz-buzz-functional/src/fizz-buzz-generator.js
@@ -1,12 +1,13 @@
 export let generateFizzBuzz = number => {
     let result = ''
-    const rules = [
-        {appliesTo: isDivisibleBy(3, number), result: 'Fizz'},
-        {appliesTo: isDivisibleBy(5, number), result: 'Buzz'},
-    ]
-    for (const r of rules)
-        if (true == r.appliesTo) result += ((result) ? '-' : '') + r.result
-
+    let r1 = {appliesTo: isDivisibleBy(3, number), result: 'Fizz'}
+    if (true === r1.appliesTo) {
+        result += ((result) ? '-' : '') + r1.result
+    }
+    let r2 = {appliesTo: isDivisibleBy(5, number), result: 'Buzz'}
+    if (true === r2.appliesTo) {
+        result += ((result) ? '-' : '') + r2.result
+    }
     return result || number.toString()
 }
 
