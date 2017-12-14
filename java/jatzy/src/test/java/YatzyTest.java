0f9aeeae92ea0afcae8dac72200ba3c6e3ed30da
Christoph Welcz <christoph@welcz.de>
2017-12-14 13:45:16 +0100

    basic gradle setup with JUnit 5.x and AssertJ

---
+++ java/jatzy/src/test/java/YatzyTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class YatzyTest {
+    @Test
+    public void basic_setup_test() {
+        assertThat(true).isTrue();
+    }
+}
