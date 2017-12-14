ccc1fa449cf0a084d140ce0838f7f4cd999d0ab3
Christoph Welcz <christoph@welcz.de>
2017-12-14 13:55:20 +0100

    fixed project name typo ;-)

---
+++ java/yahtzee/src/test/java/YahtzeeTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class YahtzeeTest {
+    @Test
+    public void basic_setup_test() {
+        assertThat(true).isTrue();
+    }
+}
