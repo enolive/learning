140b513bca830f53852b351781951823fa821e0a
Christoph Welcz <christoph@welcz.de>
2017-12-14 19:30:29 +0100

    now somehow happy with the implementation

--- java/yahtzee/src/main/java/Yahtzee.java
+++ java/yahtzee/src/main/java/Yahtzee.java
@@ -34,26 +34,26 @@ public class Yahtzee {
     }
 
     public int pair() {
-        return pairs()
-                .max(Comparator.comparing(Map.Entry::getKey))
-                .map(Yahtzee::eyesTimesOccurrence)
-                .orElse(0L)
-                .intValue();
+        return distinctEyes().filter(Yahtzee::isPair)
+                             .max(Comparator.comparing(Map.Entry::getKey))
+                             .map(Yahtzee::eyesTimesOccurrence)
+                             .orElse(0L)
+                             .intValue();
     }
 
     public int twoPairs() {
-        long[] pairValues = pairs()
-                .mapToLong(Yahtzee::eyesTimesOccurrence)
-                .toArray();
+        long[] pairValues = distinctEyes().filter(Yahtzee::isPair)
+                                          .mapToLong(Yahtzee::eyesTimesOccurrence)
+                                          .toArray();
         return pairValues.length != 2
                 ? 0
                 : (int) Arrays.stream(pairValues).sum();
     }
 
     public int triple() {
-        return (int) triples()
-                .mapToLong(Yahtzee::eyesTimesOccurrence)
-                .sum();
+        return (int) distinctEyes().filter(Yahtzee::isTriple)
+                                   .mapToLong(Yahtzee::eyesTimesOccurrence)
+                                   .sum();
     }
 
     private static boolean isPair(Map.Entry<Integer, Long> e) {
@@ -68,26 +68,14 @@ public class Yahtzee {
         return e.getValue() == 3;
     }
 
-    private Stream<Map.Entry<Integer, Long>> triples() {
-        return distinctEyes()
-                .entrySet()
-                .stream()
-                .filter(Yahtzee::isTriple);
-    }
-
-    private Stream<Map.Entry<Integer, Long>> pairs() {
-        return distinctEyes()
-                .entrySet()
-                .stream()
-                .filter(Yahtzee::isPair);
-    }
-
-    private Map<Integer, Long> distinctEyes() {
+    private Stream<Map.Entry<Integer, Long>> distinctEyes() {
         return Arrays
                 .stream(dices)
                 .boxed()
                 .collect(Collectors.groupingBy(
-                        Function.identity(), Collectors.counting()));
+                        Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream();
     }
 
     private int countEyesFor(int whichEye) {
