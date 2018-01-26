07f8e0ef5a38a3bf7a5cd639a49ad275a63fba08
Christoph Welcz <christoph@welcz.de>
2018-01-26 20:06:43 +0100

    converted kata to gradle

--- java/demo-kata/src/test/java/FizzBuzzTest.java
+++ java/demo-kata/src/test/java/FizzBuzzTest.java
@@ -1,10 +1,10 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class FizzBuzzTest {
+class FizzBuzzTest {
     @Test
-    public void testSomething() {
+    void testSomething() {
         assertThat(true).isFalse();
     }
 }
