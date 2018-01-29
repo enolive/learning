90389003fe26cc5d63897356e6bcfdc45682af37
Christoph Welcz <christoph@welcz.de>
2018-01-29 20:55:05 +0100

    to be fair, converted all expressions to blocks

--- kotlin/spring/src/main/kotlin/hello/FizzBuzzInKotlin.kt
+++ kotlin/spring/src/main/kotlin/hello/FizzBuzzInKotlin.kt
@@ -9,23 +9,28 @@ class FizzBuzzInKotlin : FizzBuzzService {
             Rule(isDivisibleBy(5), "Buzz")
     )
 
-    override fun calculateUpTo(limit: Int) =
-            generateSequence(1) { number -> number + 1 }
-                    .map { calculate(it) }
-                    .take(limit)
-                    .toList()
-                    .toTypedArray()
+    override fun calculateUpTo(limit: Int): Array<String> {
+        return generateSequence(1) { number -> number + 1 }
+                .map { calculate(it) }
+                .take(limit)
+                .toList()
+                .toTypedArray()
+    }
 
-    private fun isDivisibleBy(divisor: Int) =
-            { number: Int -> number % divisor == 0 }
+    private fun isDivisibleBy(divisor: Int): (Int) -> Boolean {
+        return { number: Int -> number % divisor == 0 }
+    }
 
     private data class Rule(val appliesTo: (Int) -> Boolean, val result: String)
 
-    private fun calculate(number: Int) = rules
-            .filter { it.appliesTo(number) }
-            .joinToString("-") { it.result }
-            .orIfEmpty { number.toString() }
+    private fun calculate(number: Int): String {
+        return rules
+                .filter { it.appliesTo(number) }
+                .joinToString("-") { it.result }
+                .orIfEmpty { number.toString() }
+    }
 
-    private fun String.orIfEmpty(alternative: () -> String) = 
-            if (isEmpty()) alternative() else this
+    private fun String.orIfEmpty(alternative: () -> String): String {
+        return if (isEmpty()) alternative() else this
+    }
 }
