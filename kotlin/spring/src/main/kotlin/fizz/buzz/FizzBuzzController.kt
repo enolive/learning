94170e6282466235408f4983180f79b7109b2008
Christoph Welcz <christoph@welcz.de>
2018-02-01 14:53:02 +0100

    introduced handler for invalid parameter exceptions

--- kotlin/spring/src/main/kotlin/fizz/buzz/FizzBuzzController.kt
+++ kotlin/spring/src/main/kotlin/fizz/buzz/FizzBuzzController.kt
@@ -1,13 +1,18 @@
 package fizz.buzz
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
 
+@CrossOrigin(origins = ["http://localhost:4200"])
 @RestController
 class FizzBuzzController @Autowired
 constructor(private val fizzBuzz: FizzBuzzService) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST, reason = "invalid parameters")
+    @ExceptionHandler(IllegalArgumentException::class)
+    fun handleIllegalArguments() {
+    }
+    
     @RequestMapping("/fizz-buzz/numbers/{limit}")
     fun getNumbers(@PathVariable("limit") limit: Int): Array<String> {
         require(limit in 1..1000) { "limit must be between 1 and 1000" }
