262efb709bb072e8f9ad7033d3951915fe4721c9
Christoph Welcz <christoph@welcz.de>
2018-05-14 23:13:07 +0200

    refactored first test

--- java/roman-numbers/src/test/java/RomanToArabicTest.java
+++ java/roman-numbers/src/test/java/RomanToArabicTest.java
@@ -1,13 +1,19 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RomanToArabicTest {
+
+    private RomanConverter converter;
+
+    @BeforeEach
+    void setUp() {
+        converter = new RomanConverter();
+    }
+
     @Test
-    void it_should_convert_I_to_1() {
-        var converter = new RomanConverter();
-        var input = "I";
-        var result = converter.toArabic(input);
-        assertThat(result).isEqualTo(1);
+    void it_should_convert_Is_to_1s() {
+        assertThat(converter.toArabic("I")).isEqualTo(1);
     }
 }
