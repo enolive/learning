35c8156f622bc8453c65386c8e00c4011bab6fc2
Christoph Welcz <christoph@welcz.de>
2018-05-15 00:03:02 +0200

    refactored acc a bit

--- java/roman-numbers/src/main/java/RomanToArabicConversion.java
+++ java/roman-numbers/src/main/java/RomanToArabicConversion.java
@@ -2,30 +2,20 @@ class RomanToArabicConversion {
     private String input;
     private int result;
 
-    public RomanToArabicConversion(String input) {
+    RomanToArabicConversion(String input) {
         this(input, 0);
     }
 
-    public RomanToArabicConversion(String input, int result) {
+    private RomanToArabicConversion(String input, int result) {
         this.input = input;
         this.result = result;
     }
 
-    public int getResult() {
+    int getResult() {
         return result;
     }
 
-    public RomanToArabicConversion apply(String roman, int arabic) {
-        var newResult = result;
-        var newInput = input;
-        while (newInput.startsWith(roman)) {
-            newResult = newResult + arabic;
-            newInput = newInput.substring(roman.length());
-        }
-        return new RomanToArabicConversion(newInput, newResult);
-    }
-
-    public RomanToArabicConversion apply(Rule rule) {
+    RomanToArabicConversion apply(Rule rule) {
         var newResult = result;
         var newInput = input;
         while (newInput.startsWith(rule.getRoman())) {
