2dd42dcf5fb960a397863c72634d5efc6a3f3fe8
Christoph Welcz <christoph@welcz.de>
2018-05-22 22:45:23 +0200

    created some parameterized tests

--- java/demo-kata/src/test/java/JustATest.java
+++ java/demo-kata/src/test/java/JustATest.java
@@ -1,13 +1,27 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Just a demo")
 class JustATest {
+
     @DisplayName("framework should work")
     @Test
     void testSomething() {
         assertThat(true).isFalse();
+    }
+
+    @ParameterizedTest(name = "{0} -> {1}")
+    @CsvSource(value = {
+            "5, bar",
+            "4, bla",
+            "42, the answer",
+    })
+    void testWithParams(int input, String output) {
+        assertThat(input).isEqualTo(4);
+        assertThat(output).isEqualTo("bla");
     }
 }
