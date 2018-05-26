9891ac847d6baf49b324fad934e6de601d5197c9
Christoph Welcz <christoph@welcz.de>
2018-05-26 13:29:15 +0200

    restored demo kata after an accidental commit ;-)

---
+++ java/demo-kata/src/test/java/JustATest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class JustATest {
+    @Test
+    void frameworkShouldWork() {
+        assertThat(true).isFalse();
+    }
+}
