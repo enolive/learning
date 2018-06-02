9fa302460ebe3a37a189280baee9976461cf5b2d
Christoph Welcz <christoph@welcz.de>
2018-06-02 12:24:11 +0200

    removed old roman numbers impl 2

--- java/RomanNumbers/src/main/java/de/welcz/RomanToArabicConverter.java
+++
@@ -1,9 +0,0 @@
-package de.welcz;
-
-public class RomanToArabicConverter {
-    public int toArabic(String roman) {
-        return AllDigits.of(roman)
-                .mapToInt(Digit::getArabicRepresentation)
-                .sum();
-    }
-}
