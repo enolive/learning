3e77424adf2113dbb24644705134e35d258179ed
Christoph Welcz <christoph@welcz.de>
2018-06-03 15:15:26 +0200

    converted lambdas to methods

--- java/fizzbuzz-vavr/src/main/java/FizzBuzz.java
+++ java/fizzbuzz-vavr/src/main/java/FizzBuzz.java
@@ -1,28 +1,30 @@
 import io.vavr.Function1;
-import io.vavr.Function2;
 import io.vavr.collection.Stream;
 
 class FizzBuzz {
 
     private final Function1<Integer, String> seq = sequence().memoized();
 
+    private static String combine(String a, String b) {
+        if (a.isEmpty()) {
+            return b;
+        }
+        if (b.isEmpty()) {
+            return a;
+        }
+        return a + "-" + b;
+    }
+
+    private static String choice(String a, String b) {
+        return a.isEmpty() ? b : a;
+    }
+
     Stream<String> sequence() {
-        var fizzes = Stream.of("", "", "Fizz").cycle();
-        var buzzes = Stream.of("", "", "", "", "Buzz").cycle();
-        var numbers = Stream.from(1).map(n -> Integer.toString(n));
-        Function2<String, String, String> combine = (a, b) -> {
-            if (a.isEmpty()) {
-                return b;
-            }
-            if (b.isEmpty()) {
-                return a;
-            }
-            return a + "-" + b;
-        };
-        Function2<String, String, String> choice = (a, b) ->
-                a.isEmpty() ? b : a;
-        var words = fizzes.zipWith(buzzes, combine);
-        return words.zipWith(numbers, choice);
+        final var fizzes = Stream.of("", "", "Fizz").cycle();
+        final var buzzes = Stream.of("", "", "", "", "Buzz").cycle();
+        final var numbers = Stream.from(1).map(n -> Integer.toString(n));
+        final var words = fizzes.zipWith(buzzes, FizzBuzz::combine);
+        return words.zipWith(numbers, FizzBuzz::choice);
     }
 
     String single(int number) {
