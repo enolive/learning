c0ddb992a1ae3f51752a07dc66b3025b0ff0609f
Christoph Welcz <christoph@welcz.de>
2018-06-26 23:13:01 +0200

    adding one book

--- typescript/potter/src/basket.ts
+++ typescript/potter/src/basket.ts
@@ -1,7 +1,15 @@
 import Big from "big.js";
+import {Book} from "./book";
+import {Bundle} from "./bundle";
 
 export class Basket {
+    private bundles: Bundle = new Bundle();
+
     get price(): Big {
-        return new Big(8);
+        return this.bundles.price;
+    }
+
+    add(book: Book) {
+        this.bundles.add(book);
     }
 }
--- typescript/potter/test/basket.spec.ts
+++ typescript/potter/test/basket.spec.ts
@@ -1,6 +1,7 @@
 import {expect} from "chai";
 import {beforeEach, describe, it} from "mocha";
 import {Basket} from "../src/basket";
+import {Book} from "../src/book";
 
 describe("Basket of books", () => {
     let basket: Basket;
@@ -10,6 +11,11 @@ describe("Basket of books", () => {
     });
 
     it("should calculate the price for empty", () => {
+        expect(basket.price.toFixed(2)).to.equal("0.00");
+    });
+
+    it("should calculate the price for one book", () => {
+        basket.add(new Book(1));
         expect(basket.price.toFixed(2)).to.equal("8.00");
     });
 });
