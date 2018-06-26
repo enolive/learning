3627d4c42cc3cfb5b1e9502b49715464a476209e
Christoph Welcz <christoph@welcz.de>
2018-06-26 22:36:40 +0200

    calculating discount

--- typescript/potter/src/bundle.ts
+++ typescript/potter/src/bundle.ts
@@ -1,7 +1,20 @@
+import Big from "big.js";
 import {Book} from "./book";
 
 export class Bundle {
     private books = new Map<number, Book>();
+    get discount(): number {
+        switch (this.size) {
+            case 2:
+                return 0.95;
+            default:
+                return 1.0;
+        }
+    }
+
+    get price(): Big {
+        return new Big(8).mul(this.size).mul(this.discount);
+    }
 
     get size(): number {
         return this.books.size;
--- typescript/potter/test/bundle.spec.ts
+++ typescript/potter/test/bundle.spec.ts
@@ -10,24 +10,39 @@ describe("Bundle of Books", () => {
         bundle = new Bundle();
     });
 
-    it("should be initially empty", () => {
-        expect(bundle.size).to.equal(0);
+    describe("adding books", () => {
+        it("should be initially empty", () => {
+            expect(bundle.size).to.equal(0);
+        });
+
+        it("should allow to add a book", () => {
+            bundle.add(new Book(1));
+            expect(bundle.size).to.equal(1);
+        });
+
+        it("should allow multiple different books", () => {
+            bundle.add(new Book(1));
+            bundle.add(new Book(2));
+            expect(bundle.size).to.equal(2);
+        });
+
+        it("should ignore adding the same book multiple times", () => {
+            bundle.add(new Book(1));
+            bundle.add(new Book(1));
+            expect(bundle.size).to.equal(1);
+        });
     });
 
-    it("should allow to add a book", () => {
-        bundle.add(new Book(1));
-        expect(bundle.size).to.equal(1);
-    });
+    describe("calculating price", () => {
+        it("should calculate the book price for 1", () => {
+            bundle.add(new Book(1));
+            expect(bundle.price.toFixed(2)).to.equal("8.00");
+        });
 
-    it("should allow multiple different books", () => {
-        bundle.add(new Book(1));
-        bundle.add(new Book(2));
-        expect(bundle.size).to.equal(2);
-    });
-
-    it("should ignore adding the same book multiple times", () => {
-        bundle.add(new Book(1));
-        bundle.add(new Book(1));
-        expect(bundle.size).to.equal(1);
+        it("should calculate the book price for 2", () => {
+            bundle.add(new Book(1));
+            bundle.add(new Book(2));
+            expect(bundle.price.toFixed(2)).to.equal("15.20");
+        });
     });
 });
