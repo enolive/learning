285d91bfee416ad0b2b52ae3f3b6d9488e2ac752
Christoph Welcz <christoph@welcz.de>
2018-07-05 17:28:05 +0200

    generalized fizz

--- groovy/fizzbuzz/src/test/groovy/FizzBuzzSpec.groovy
+++ groovy/fizzbuzz/src/test/groovy/FizzBuzzSpec.groovy
@@ -10,16 +10,17 @@ class FizzBuzzSpec extends Specification {
         2     || "2"
     }
 
-    def "3 should be returned as Fizz"() {
+    def "numbers divisible by 3 should be returned as Fizz"() {
         expect:
         calculate(input) == expected
         where:
         input || expected
         3     || "Fizz"
+        6     || "Fizz"
     }
 
     def calculate(int input) {
-        if (input == 3) {
+        if (input % 3 == 0) {
             return "Fizz"
         }
         input.toString()
