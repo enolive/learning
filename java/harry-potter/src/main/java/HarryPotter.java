a8c167e2d45761bfee02e7b5bd962063efd470c0
Christoph Welcz <christoph@welcz.de>
2018-07-16 20:03:33 +0200

    solved get group prices

--- java/harry-potter/src/main/java/HarryPotter.java
+++ java/harry-potter/src/main/java/HarryPotter.java
@@ -2,9 +2,17 @@ import io.vavr.collection.List;
 
 import java.math.BigDecimal;
 
+import static io.vavr.API.*;
+
 class HarryPotter {
     static BigDecimal getGroupPrice(int numberOfBooks) {
-        return BigDecimal.ZERO;
+        return Match(numberOfBooks).of(
+                Case($(1), BigDecimal.valueOf(8.0)),
+                Case($(2), BigDecimal.valueOf(15.2)),
+                Case($(3), BigDecimal.valueOf(21.6)),
+                Case($(4), BigDecimal.valueOf(25.6)),
+                Case($(5), BigDecimal.valueOf(30.0))
+        );
     }
 
     static List<BookSet> getBookSets(List<Book> books) {
