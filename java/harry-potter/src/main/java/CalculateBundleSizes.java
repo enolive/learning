73484a4728bffc8ef795d21add753c75c7ab3910
Christoph Welcz <christoph@welcz.de>
2018-07-16 22:40:17 +0200

    changed all mutable fields to method object parameters

--- java/harry-potter/src/main/java/CalculateBundleSizes.java
+++ java/harry-potter/src/main/java/CalculateBundleSizes.java
@@ -3,22 +3,26 @@ import io.vavr.collection.Stream;
 
 class CalculateBundleSizes {
     private final List<BookSet> bookSets;
+    private final int alreadyUsed;
+    private final Stream<Integer> calculatedSizes;
 
     CalculateBundleSizes(List<BookSet> bookSets) {
+        this(bookSets, 0, Stream.empty());
+    }
+
+    private CalculateBundleSizes(List<BookSet> bookSets, int alreadyUsed, Stream<Integer> calculatedSizes) {
         this.bookSets = bookSets;
+        this.alreadyUsed = alreadyUsed;
+        this.calculatedSizes = calculatedSizes;
     }
 
     Stream<Integer> invoke() {
-        return invoke(0, Stream.empty());
-    }
-
-    private Stream<Integer> invoke(int alreadyUsed, Stream<Integer> acc) {
         if (bookSets.isEmpty()) {
-            return acc;
+            return calculatedSizes;
         }
         final var numberOfCurrentBooks = bookSets.head().getCount();
         final var sizeOfCurrentBundle = numberOfCurrentBooks - alreadyUsed;
         final var remainingSets = bookSets.tail();
-        return new CalculateBundleSizes(remainingSets).invoke(numberOfCurrentBooks, acc.prepend(sizeOfCurrentBundle));
+        return new CalculateBundleSizes(remainingSets, numberOfCurrentBooks, calculatedSizes.prepend(sizeOfCurrentBundle)).invoke();
     }
 }
