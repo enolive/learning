756ae0aec8c3007109f4ae426401c4d0278a084e
Christoph Welcz <christoph@welcz.de>
2018-08-07 19:28:46 +0200

    parameterized moving forward

--- kotlin/mars-rover/src/test/kotlin/RoverTest.kt
+++ kotlin/mars-rover/src/test/kotlin/RoverTest.kt
@@ -7,28 +7,16 @@ import org.junit.jupiter.params.provider.CsvSource
 class RoverTest {
     @Nested
     inner class `Moving forward` {
-        @Test
-        internal fun `it should move to the north`() {
-            assertThat(Rover(Position(1, 1), Bearing.NORTH).move(Command.FORWARD))
-                    .isEqualTo(Rover(Position(1, 0), Bearing.NORTH))
-        }
-
-        @Test
-        internal fun `it should move to the south`() {
-            assertThat(Rover(Position(1, 1), Bearing.SOUTH).move(Command.FORWARD))
-                    .isEqualTo(Rover(Position(1, 2), Bearing.SOUTH))
-        }
-
-        @Test
-        internal fun `it should move to the east`() {
-            assertThat(Rover(Position(1, 1), Bearing.EAST).move(Command.FORWARD))
-                    .isEqualTo(Rover(Position(0, 1), Bearing.EAST))
-        }
-
-        @Test
-        internal fun `it should move to the west`() {
-            assertThat(Rover(Position(1, 1), Bearing.WEST).move(Command.FORWARD))
-                    .isEqualTo(Rover(Position(2, 1), Bearing.WEST))
+        @ParameterizedTest
+        @CsvSource(value = [
+            "NORTH, 1, 0",
+            "SOUTH, 1, 2",
+            "EAST, 0, 1",
+            "WEST, 2, 1"
+        ])
+        internal fun `it should move forward`(initialBearing: Bearing, expectedX: Int, expectedY: Int) {
+            assertThat(Rover(Position(1, 1), initialBearing).move(Command.FORWARD))
+                    .isEqualTo(Rover(Position(expectedX, expectedY), initialBearing))
         }
     }
 
