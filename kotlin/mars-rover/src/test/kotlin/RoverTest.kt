be147b5503f939e5404fad5c304838d276dcbd9d
Christoph Welcz <christoph@welcz.de>
2018-08-07 18:30:17 +0200

    introduced nested testing

--- kotlin/mars-rover/src/test/kotlin/RoverTest.kt
+++ kotlin/mars-rover/src/test/kotlin/RoverTest.kt
@@ -1,23 +1,27 @@
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class RoverTest {
-    @Test
-    internal fun `it should move to the north`() {
-        assertThat(Rover(Position(1, 1), Bearing.NORTH).move(Command.FORWARD))
-                .isEqualTo(Rover(Position(1, 0), Bearing.NORTH))
-    }
+    @Nested
+    inner class `Moving forward` {
+        @Test
+        internal fun `it should move to the north`() {
+            assertThat(Rover(Position(1, 1), Bearing.NORTH).move(Command.FORWARD))
+                    .isEqualTo(Rover(Position(1, 0), Bearing.NORTH))
+        }
 
-    @Test
-    internal fun `it should move to the south`() {
-        assertThat(Rover(Position(1, 1), Bearing.SOUTH).move(Command.FORWARD))
-                .isEqualTo(Rover(Position(1, 2), Bearing.SOUTH))
-    }
+        @Test
+        internal fun `it should move to the south`() {
+            assertThat(Rover(Position(1, 1), Bearing.SOUTH).move(Command.FORWARD))
+                    .isEqualTo(Rover(Position(1, 2), Bearing.SOUTH))
+        }
 
-    @Test
-    internal fun `it should move to the east`() {
-        assertThat(Rover(Position(1, 1), Bearing.EAST).move(Command.FORWARD))
-                .isEqualTo(Rover(Position(0, 1), Bearing.EAST))
+        @Test
+        internal fun `it should move to the east`() {
+            assertThat(Rover(Position(1, 1), Bearing.EAST).move(Command.FORWARD))
+                    .isEqualTo(Rover(Position(0, 1), Bearing.EAST))
+        }
     }
 }
 
