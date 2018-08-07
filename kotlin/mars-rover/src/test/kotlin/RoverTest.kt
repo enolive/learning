88bc076c5a6f4f68483cc5858aa14627c543a7ac
Christoph Welcz <christoph@welcz.de>
2018-08-07 19:30:27 +0200

    parameterized moving backwards as well

--- kotlin/mars-rover/src/test/kotlin/RoverTest.kt
+++ kotlin/mars-rover/src/test/kotlin/RoverTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.params.provider.CsvSource
 
 class RoverTest {
     @Nested
-    inner class `Moving forward` {
+    inner class `Moving forward & backward` {
         @ParameterizedTest
         @CsvSource(value = [
             "NORTH, 1, 0",
@@ -18,32 +18,17 @@ class RoverTest {
             assertThat(Rover(Position(1, 1), initialBearing).move(Command.FORWARD))
                     .isEqualTo(Rover(Position(expectedX, expectedY), initialBearing))
         }
-    }
 
-    @Nested
-    inner class `Moving backwards` {
-        @Test
-        internal fun `it should move to the north`() {
-            assertThat(Rover(Position(1, 1), Bearing.SOUTH).move(Command.BACKWARD))
-                    .isEqualTo(Rover(Position(1, 0), Bearing.SOUTH))
-        }
-
-        @Test
-        internal fun `it should move to the south`() {
-            assertThat(Rover(Position(1, 1), Bearing.NORTH).move(Command.BACKWARD))
-                    .isEqualTo(Rover(Position(1, 2), Bearing.NORTH))
-        }
-
-        @Test
-        internal fun `it should move to the east`() {
-            assertThat(Rover(Position(1, 1), Bearing.WEST).move(Command.BACKWARD))
-                    .isEqualTo(Rover(Position(0, 1), Bearing.WEST))
-        }
-
-        @Test
-        internal fun `it should move to the west`() {
-            assertThat(Rover(Position(1, 1), Bearing.EAST).move(Command.BACKWARD))
-                    .isEqualTo(Rover(Position(2, 1), Bearing.EAST))
+        @ParameterizedTest
+        @CsvSource(value = [
+            "NORTH, 1, 2",
+            "SOUTH, 1, 0",
+            "EAST, 2, 1",
+            "WEST, 0, 1"
+        ])
+        internal fun `it should move backwards`(initialBearing: Bearing, expectedX: Int, expectedY: Int) {
+            assertThat(Rover(Position(1, 1), initialBearing).move(Command.BACKWARD))
+                    .isEqualTo(Rover(Position(expectedX, expectedY), initialBearing))
         }
     }
 
