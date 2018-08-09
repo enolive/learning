58203e9701545b5f4239d1e17ed20e90a86f762b
Christoph Welcz <christoph@welcz.de>
2018-08-09 20:20:20 +0200

    initial test for price of a group

---
+++ typescript/harry-potter/test/potter.spec.ts
@@ -0,0 +1,21 @@
+import {expect} from "chai";
+
+function priceBundle(bundleSize: number) {
+    return [8, 15.2, 21.6, 25.6, 30][bundleSize - 1];
+}
+
+describe('Harry Potter Kata', () => {
+    describe('bundle price', function () {
+        [
+            {size: 1, price: 8},
+            {size: 2, price: 15.2},
+            {size: 3, price: 21.6},
+            {size: 4, price: 25.6},
+            {size: 5, price: 30.0},
+        ].forEach(value =>
+            it(`should return the price of ${value.price} for ${value.size} books`, () => {
+                expect(priceBundle(value.size)).to.equal(value.price);
+            })
+        );
+    });
+});
