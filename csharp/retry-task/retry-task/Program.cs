87a7e1a335fe8fdbcb0f88764988f092d890a42b
Christoph Welcz <christoph@welcz.de>
2018-08-19 18:17:23 +0200

    implemented a delay

--- csharp/retry-task/retry-task/Program.cs
+++ csharp/retry-task/retry-task/Program.cs
@@ -7,9 +7,10 @@ namespace retry_task
     {
         private static readonly Random Random = new Random();
 
-        static void Main(string[] args)
+        static void Main()
         {
-            Retry(DoSomething, 5).Wait();
+            var configuration = new RetryConfiguration(5, 500.MilliSeconds());
+            Retry(DoSomething, configuration).Wait();
         }
 
         private static Unit DoSomething()
@@ -26,11 +27,12 @@ namespace retry_task
 
         private static bool ShouldFail()
         {
-            return Random.Next(0, 2) == 0;
+            return Random.Next(0, 3) != 0;
         }
 
-        private static async Task<T> Retry<T>(Func<T> func, int retryCount)
+        private static async Task<T> Retry<T>(Func<T> func, RetryConfiguration configuration)
         {
+            var remainingRetries = configuration.Retries;
             while (true)
             {
                 try
@@ -38,15 +40,36 @@ namespace retry_task
                     var result = await Task.Run(func);
                     return result;
                 }
-                catch when (retryCount-- > 0)
+                catch when (remainingRetries-- > 0)
                 {
+                    await Task.Delay(configuration.DelayBeforeRetry);
                 }
             }
+        }
+    }
+
+    internal class RetryConfiguration
+    {
+        public int Retries { get; }
+        public TimeSpan DelayBeforeRetry { get; }
+
+        public RetryConfiguration(int retries, TimeSpan delayBeforeRetry)
+        {
+            Retries = retries;
+            DelayBeforeRetry = delayBeforeRetry;
         }
     }
 
     internal class Unit
     {
         public static Unit Value { get; } = new Unit();
+    }
+
+    internal static class TimespanExtensions
+    {
+        public static TimeSpan MilliSeconds(this int milliSeconds)
+        {
+            return TimeSpan.FromMilliseconds(milliSeconds);
+        }
     }
 }
