9f38e3d674d79a88c1fccbd7396c2a0b7d4a9e7d
Christoph Welcz <christoph@welcz.de>
2018-09-06 21:36:05 +0200

    implemented multiple commands and translation from string

--- java/mars-rover/src/main/java/Rover.java
+++ java/mars-rover/src/main/java/Rover.java
@@ -1,29 +1,63 @@
+import io.vavr.Tuple;
+import io.vavr.collection.CharSeq;
+import io.vavr.collection.HashMap;
+
+import java.text.MessageFormat;
+import java.util.function.Function;
+
 class Rover {
+    private final static HashMap<Character, Function<Rover, Rover>> COMMAND_MAP =
+            HashMap.ofEntries(
+                    Tuple.of('f', Rover::forward),
+                    Tuple.of('b', Rover::backward),
+                    Tuple.of('l', Rover::turnLeft),
+                    Tuple.of('r', Rover::turnRight)
+            );
     private final Position position;
     private final Orientation orientation;
+
+    Rover() {
+        this(new Position(0, 0), Orientation.NORTH);
+    }
 
     Rover(Position position, Orientation orientation) {
         this.position = position;
         this.orientation = orientation;
     }
 
-    Rover() {
-        this(new Position(0, 0), Orientation.NORTH);
-    }
-
-    Rover forward() {
-        return new Rover(position.nextForward(orientation), orientation);
+    Rover backward() {
+        return turnRight().turnRight().forward().turnRight().turnRight();
     }
 
     Rover turnRight() {
         return new Rover(position, orientation.nextRight());
     }
 
-    Rover backward() {
-        return turnRight().turnRight().forward().turnRight().turnRight();
+    Rover forward() {
+        return new Rover(position.nextForward(orientation), orientation);
     }
 
     Rover turnLeft() {
         return turnRight().turnRight().turnRight();
+    }
+
+    Rover execute(String commandList) {
+        return CharSeq.of(commandList)
+                      .map(this::translate)
+                      .foldLeft(this, this::executeAction);
+    }
+
+    private Rover executeAction(Rover current, Function<Rover, Rover> transformRover) {
+        return transformRover.apply(current);
+    }
+
+    private Function<Rover, Rover> translate(char singleCommand) {
+        return COMMAND_MAP.get(singleCommand)
+                          .getOrElseThrow(() -> illegalCommand(singleCommand));
+    }
+
+    private IllegalArgumentException illegalCommand(Character first) {
+        return new IllegalArgumentException(
+                MessageFormat.format("wrong command {0}", first));
     }
 }
--- java/mars-rover/src/test/java/RoverTest.java
+++ java/mars-rover/src/test/java/RoverTest.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -5,70 +7,120 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class RoverTest {
-    @Test
-    void defaultRover() {
-        var expected = new Rover(new Position(0, 0), Orientation.NORTH);
-        var rover = new Rover();
-        assertThat(rover).isEqualToComparingFieldByFieldRecursively(expected);
+    @Nested
+    class Construction {
+        @Test
+        void defaultRover() {
+            var expected = new Rover(new Position(0, 0), Orientation.NORTH);
+            var rover = new Rover();
+            assertThat(rover).isEqualToComparingFieldByFieldRecursively(expected);
+        }
     }
 
-    @ParameterizedTest
-    @CsvSource(value = {
-            "14, 45, NORTH, 14, 44",
-            "33, 13, NORTH, 33, 12",
-            "5, 11, SOUTH, 5, 12",
-            "22, 1, EAST, 23, 1",
-            "105, 12, WEST, 104, 12",
-    })
-    void forward(int startX, int startY, Orientation orientation, int expectedX, int expectedY) {
-        final var expectedPosition = new Position(expectedX, expectedY);
-        final var startPosition = new Position(startX, startY);
-        final var expected = new Rover(expectedPosition, orientation);
-        final var rover = new Rover(startPosition, orientation);
-        assertThat(rover.forward()).isEqualToComparingFieldByFieldRecursively(expected);
+    @Nested
+    class Moving {
+        @ParameterizedTest
+        @CsvSource(value = {
+                "14, 45, NORTH, 14, 44",
+                "33, 13, NORTH, 33, 12",
+                "5, 11, SOUTH, 5, 12",
+                "22, 1, EAST, 23, 1",
+                "105, 12, WEST, 104, 12",
+        })
+        void forward(int startX, int startY, Orientation orientation, int expectedX, int expectedY) {
+            final var expectedPosition = new Position(expectedX, expectedY);
+            final var startPosition = new Position(startX, startY);
+            final var expected = new Rover(expectedPosition, orientation);
+            final var rover = new Rover(startPosition, orientation);
+            assertThat(rover.forward()).isEqualToComparingFieldByFieldRecursively(expected);
+        }
+
+        @ParameterizedTest
+        @CsvSource(value = {
+                "14, 45, NORTH, 14, 46",
+                "33, 13, NORTH, 33, 14",
+                "5, 11, SOUTH, 5, 10",
+                "22, 1, EAST, 21, 1",
+                "105, 12, WEST, 106, 12",
+        })
+        void backward(int startX, int startY, Orientation orientation, int expectedX, int expectedY) {
+            final var expectedPosition = new Position(expectedX, expectedY);
+            final var startPosition = new Position(startX, startY);
+            final var expected = new Rover(expectedPosition, orientation);
+            final var rover = new Rover(startPosition, orientation);
+            assertThat(rover.backward()).isEqualToComparingFieldByFieldRecursively(expected);
+        }
     }
 
-    @ParameterizedTest
-    @CsvSource(value = {
-            "14, 45, NORTH, 14, 46",
-            "33, 13, NORTH, 33, 14",
-            "5, 11, SOUTH, 5, 10",
-            "22, 1, EAST, 21, 1",
-            "105, 12, WEST, 106, 12",
-    })
-    void backward(int startX, int startY, Orientation orientation, int expectedX, int expectedY) {
-        final var expectedPosition = new Position(expectedX, expectedY);
-        final var startPosition = new Position(startX, startY);
-        final var expected = new Rover(expectedPosition, orientation);
-        final var rover = new Rover(startPosition, orientation);
-        assertThat(rover.backward()).isEqualToComparingFieldByFieldRecursively(expected);
+    @Nested
+    class Turning {
+        @ParameterizedTest
+        @CsvSource(value = {
+                "NORTH, EAST",
+                "EAST, SOUTH",
+                "SOUTH, WEST",
+                "WEST, NORTH",
+        })
+        void turnRight(Orientation start, Orientation end) {
+            final var position = new Position(12, 33);
+            final var rover = new Rover(position, start);
+            final var expected = new Rover(position, end);
+            assertThat(rover.turnRight()).isEqualToComparingFieldByFieldRecursively(expected);
+        }
+
+        @ParameterizedTest
+        @CsvSource(value = {
+                "NORTH, WEST",
+                "WEST, SOUTH",
+                "SOUTH, EAST",
+                "EAST, NORTH",
+        })
+        void turnLeft(Orientation start, Orientation end) {
+            final var position = new Position(12, 33);
+            final var rover = new Rover(position, start);
+            final var expected = new Rover(position, end);
+            assertThat(rover.turnLeft()).isEqualToComparingFieldByFieldRecursively(expected);
+        }
     }
 
-    @ParameterizedTest
-    @CsvSource(value = {
-            "NORTH, EAST",
-            "EAST, SOUTH",
-            "SOUTH, WEST",
-            "WEST, NORTH",
-    })
-    void turnRight(Orientation start, Orientation end) {
-        final var position = new Position(12, 33);
-        final var rover = new Rover(position, start);
-        final var expected = new Rover(position, end);
-        assertThat(rover.turnRight()).isEqualToComparingFieldByFieldRecursively(expected);
-    }
+    @Nested
+    class Commands {
 
-    @ParameterizedTest
-    @CsvSource(value = {
-            "NORTH, WEST",
-            "WEST, SOUTH",
-            "SOUTH, EAST",
-            "EAST, NORTH",
-    })
-    void turnLeft(Orientation start, Orientation end) {
-        final var position = new Position(12, 33);
-        final var rover = new Rover(position, start);
-        final var expected = new Rover(position, end);
-        assertThat(rover.turnLeft()).isEqualToComparingFieldByFieldRecursively(expected);
+        private Rover rover;
+
+        @BeforeEach
+        void setUp() {
+            rover = new Rover();
+        }
+
+        @Test
+        void forward() {
+            final var expected = new Rover(new Position(0, -1), Orientation.NORTH);
+            assertThat(rover.execute("f")).isEqualToComparingFieldByFieldRecursively(expected);
+        }
+
+        @Test
+        void backward() {
+            final var expected = new Rover(new Position(0, 1), Orientation.NORTH);
+            assertThat(rover.execute("b")).isEqualToComparingFieldByFieldRecursively(expected);
+        }
+
+        @Test
+        void turnLeft() {
+            final var expected = new Rover(new Position(0, 0), Orientation.WEST);
+            assertThat(rover.execute("l")).isEqualToComparingFieldByFieldRecursively(expected);
+        }
+
+        @Test
+        void turnRight() {
+            final var expected = new Rover(new Position(0, 0), Orientation.EAST);
+            assertThat(rover.execute("r")).isEqualToComparingFieldByFieldRecursively(expected);
+        }
+
+        @Test
+        void moveMultipleTimes() {
+            final var expected = new Rover(new Position(-4, -6), Orientation.NORTH);
+            assertThat(rover.execute("ffffrllllbbbbrrrff")).isEqualToComparingFieldByFieldRecursively(expected);
+        }
     }
 }
