23a22fdfe88744759a59e6e76764bc48d54386fb
Christoph Welcz <christoph@welcz.de>
2018-10-01 20:00:49 +0200

    fixed project

--- java/yahtzee/src/main/java/Yahtzee.java
+++ java/yahtzee/src/main/java/Yahtzee.java
@@ -13,85 +13,21 @@ public class Yahtzee {
         this.dices = dices;
     }
 
-    public int ones() {
-        return countEyesFor(1);
+    private void throwOnInvalid(int[] dices) {
+        if (!hasValidLength(dices)) {
+            throw new IllegalArgumentException("The number of dices must be 5.");
+        }
+        if (Arrays.stream(dices).anyMatch(dice -> !hasValidEyeCount(dice))) {
+            throw new IllegalArgumentException("The eye count for each dice must be between 1 and 6.");
+        }
     }
 
-    public int twos() {
-        return countEyesFor(2);
+    private boolean hasValidLength(int[] dices) {
+        return dices.length == 5;
     }
 
-    public int threes() {
-        return countEyesFor(3);
-    }
-
-    public int fours() {
-        return countEyesFor(4);
-    }
-
-    public int fives() {
-        return countEyesFor(5);
-    }
-
-    public int pair() {
-        return (int) distinctEyes().filter(Yahtzee::isPair)
-                                   .mapToLong(Yahtzee::eyesTimesOccurrence)
-                                   .max()
-                                   .orElse(0L);
-    }
-
-    public int twoPairs() {
-        long[] pairValues = distinctEyes().filter(Yahtzee::isPair)
-                                          .mapToLong(Yahtzee::eyesTimesOccurrence)
-                                          .toArray();
-        return pairValues.length != 2
-                ? 0
-                : (int) Arrays.stream(pairValues).sum();
-    }
-
-    public int threeOfAKind() {
-        return (int) distinctEyes().filter(Yahtzee::areThreeOfAKind)
-                                   .mapToLong(Yahtzee::eyesTimesOccurrence)
-                                   .findFirst()
-                                   .orElse(0L);
-    }
-
-    public int fourOfAKind() {
-        return (int) distinctEyes().filter(Yahtzee::areFourOfAKind)
-                                   .mapToLong(Yahtzee::eyesTimesOccurrence)
-                                   .findFirst()
-                                   .orElse(0L);
-    }
-
-    public int smallStraight() {
-        return dicesContainAll(1, 2, 3, 4, 5)
-                ? sumOfAllDices()
-                : 0;
-    }
-
-    public int largeStraight() {
-        return dicesContainAll(2, 3, 4, 5, 6)
-                ? sumOfAllDices()
-                : 0;
-    }
-
-    public int chance() {
-        return sumOfAllDices();
-    }
-
-    public int fullHouse() {
-        int pair = pair();
-        int triple = threeOfAKind();
-        return triple > 0 && pair > 0
-                ? pair + triple
-                : 0;
-    }
-
-    public int yahtzee() {
-        return (int) distinctEyes().filter(Yahtzee::isYahtzee)
-                                   .mapToLong(Yahtzee::eyesTimesOccurrence)
-                                   .findFirst()
-                                   .orElse(0L);
+    private boolean hasValidEyeCount(int dice) {
+        return dice >= 1 && dice <= 6;
     }
 
     private static boolean isPair(Map.Entry<Integer, Long> e) {
@@ -114,38 +50,61 @@ public class Yahtzee {
         return e.getValue() == 5;
     }
 
-    private void throwOnInvalid(int[] dices) {
-        if (!hasValidLength(dices)) {
-            throw new IllegalArgumentException("The number of dices must be 5.");
-        }
-        if (Arrays.stream(dices).anyMatch(dice -> !hasValidEyeCount(dice))) {
-            throw new IllegalArgumentException("The eye count for each dice must be between 1 and 6.");
-        }
-    }
-
-    private boolean hasValidLength(int[] dices) {
-        return dices.length == 5;
-    }
-
-    private boolean hasValidEyeCount(int dice) {
-        return dice >= 1 && dice <= 6;
+    int ones() {
+        return countEyesFor(1);
     }
 
     private int countEyesFor(int whichEye) {
-        return (int) Arrays
-                .stream(dices)
-                .filter(d -> d == whichEye)
-                .count() * whichEye;
+        return (int) Arrays.stream(dices)
+                           .filter(d -> d == whichEye)
+                           .count() * whichEye;
+    }
+
+    int twos() {
+        return countEyesFor(2);
+    }
+
+    int threes() {
+        return countEyesFor(3);
+    }
+
+    int fours() {
+        return countEyesFor(4);
+    }
+
+    int fives() {
+        return countEyesFor(5);
+    }
+
+    int twoPairs() {
+        long[] pairValues = distinctEyes().filter(Yahtzee::isPair)
+                                          .mapToLong(Yahtzee::eyesTimesOccurrence)
+                                          .toArray();
+        return pairValues.length != 2
+                ? 0
+                : (int) Arrays.stream(pairValues).sum();
     }
 
     private Stream<Map.Entry<Integer, Long>> distinctEyes() {
-        return Arrays
-                .stream(dices)
-                .boxed()
-                .collect(Collectors.groupingBy(
-                        Function.identity(), Collectors.counting()))
-                .entrySet()
-                .stream();
+        return Arrays.stream(dices)
+                     .boxed()
+                     .collect(Collectors.groupingBy(
+                             Function.identity(), Collectors.counting()))
+                     .entrySet()
+                     .stream();
+    }
+
+    int fourOfAKind() {
+        return (int) distinctEyes().filter(Yahtzee::areFourOfAKind)
+                                   .mapToLong(Yahtzee::eyesTimesOccurrence)
+                                   .findFirst()
+                                   .orElse(0L);
+    }
+
+    int smallStraight() {
+        return dicesContainAll(1, 2, 3, 4, 5)
+                ? sumOfAllDices()
+                : 0;
     }
 
     private boolean dicesContainAll(Integer... eyes) {
@@ -157,5 +116,44 @@ public class Yahtzee {
 
     private int sumOfAllDices() {
         return Arrays.stream(dices).sum();
+    }
+
+    int largeStraight() {
+        return dicesContainAll(2, 3, 4, 5, 6)
+                ? sumOfAllDices()
+                : 0;
+    }
+
+    int chance() {
+        return sumOfAllDices();
+    }
+
+    int fullHouse() {
+        int pair = pair();
+        int triple = threeOfAKind();
+        return triple > 0 && pair > 0
+                ? pair + triple
+                : 0;
+    }
+
+    int pair() {
+        return (int) distinctEyes().filter(Yahtzee::isPair)
+                                   .mapToLong(Yahtzee::eyesTimesOccurrence)
+                                   .max()
+                                   .orElse(0L);
+    }
+
+    int threeOfAKind() {
+        return (int) distinctEyes().filter(Yahtzee::areThreeOfAKind)
+                                   .mapToLong(Yahtzee::eyesTimesOccurrence)
+                                   .findFirst()
+                                   .orElse(0L);
+    }
+
+    int yahtzee() {
+        return (int) distinctEyes().filter(Yahtzee::isYahtzee)
+                                   .mapToLong(Yahtzee::eyesTimesOccurrence)
+                                   .findFirst()
+                                   .orElse(0L);
     }
 }
