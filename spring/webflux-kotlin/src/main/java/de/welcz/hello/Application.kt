55d3a414583408ad7af4f9962599685a13935f0a
Christoph Welcz <christoph@welcz.de>
2018-11-05 20:49:07 +0100

    converted Application to kotlin

---
+++ spring/webflux-kotlin/src/main/java/de/welcz/hello/Application.kt
@@ -0,0 +1,14 @@
+package de.welcz.hello
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+open class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+
+    val client = GreetingWebClient()
+    println(client.result)
+}
