4deb04475500858969306b6cfa85f9c64701f750
Christoph Welcz <christoph@welcz.de>
2018-11-05 21:07:46 +0100

    simplified router

--- spring/webflux-kotlin/src/main/java/de/welcz/hello/GreetingRouter.kt
+++ spring/webflux-kotlin/src/main/java/de/welcz/hello/GreetingRouter.kt
@@ -2,19 +2,16 @@ package de.welcz.hello
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.reactive.function.server.HandlerFunction
-import org.springframework.web.reactive.function.server.RouterFunction
-import org.springframework.web.reactive.function.server.ServerResponse
-
-import org.springframework.web.reactive.function.server.RequestPredicates.GET
-import org.springframework.web.reactive.function.server.RouterFunctions.route
+import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.server.router
 
 @Configuration
 open class GreetingRouter(private val handler: GreetingHandler) {
 
     @Bean
-    open fun composedRoutes(): RouterFunction<ServerResponse> {
-        return route<ServerResponse>(GET("/hello"), HandlerFunction<ServerResponse> { handler.hello(it) })
-                .and(route(GET("/hello/{name}"), HandlerFunction<ServerResponse> { handler.hello(it) }))
+    open fun composedRoutes() = router {
+        accept(MediaType.TEXT_PLAIN).nest {
+            (GET("/hello") or GET("/hello/{name}")).invoke(handler::hello)
+        }
     }
 }
