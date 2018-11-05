fdc173ceeb1641eb2fc17a82e5176537d8adad43
Christoph Welcz <christoph@welcz.de>
2018-11-05 20:01:28 +0100

    basic tutorial for webflux

---
+++ spring/webflux-java/src/main/java/de/welcz/hello/Application.java
@@ -0,0 +1,14 @@
+package de.welcz.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+
+        var client = new GreetingWebClient();
+        System.out.println(client.getResult());
+    }
+}
