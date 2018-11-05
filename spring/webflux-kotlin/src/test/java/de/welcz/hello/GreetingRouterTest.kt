86afe4a0d22d72e165e0efdb50b9cf73ffb1c44d
Christoph Welcz <christoph@welcz.de>
2018-11-05 21:11:51 +0100

    converted test to kotlin

---
+++ spring/webflux-kotlin/src/test/java/de/welcz/hello/GreetingRouterTest.kt
@@ -0,0 +1,41 @@
+package de.welcz.hello
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@AutoConfigureWebTestClient
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner::class)
+class GreetingRouterTest {
+    @Autowired
+    internal lateinit var client: WebTestClient
+
+    @Test
+    fun outputsHelloWithSpecifiedName() {
+        client.get()
+                .uri("/hello/Spring")
+                .accept(MediaType.TEXT_PLAIN)
+                .exchange()
+                .expectStatus().isOk
+                .expectBody(String::class.java)
+                .consumeWith<Nothing> { result -> assertThat(result.responseBody).startsWith("Hello, Spring!") }
+    }
+
+    @Test
+    fun outputsHelloWithDefault() {
+        client.get()
+                .uri("/hello")
+                .accept(MediaType.TEXT_PLAIN)
+                .exchange()
+                .expectStatus().isOk
+                .expectBody(String::class.java)
+                .consumeWith<Nothing> { result -> assertThat(result.responseBody).startsWith("Hello, World!") }
+    }
+}
