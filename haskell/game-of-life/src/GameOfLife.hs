37904c5d3633e619f7076e917135d82fef37de87
Christoph Welcz <christoph@welcz.de>
2018-11-18 22:23:34 +0100

    updated solution

--- haskell/game-of-life/src/GameOfLife.hs
+++ haskell/game-of-life/src/GameOfLife.hs
@@ -1,73 +1,96 @@
 {-# LANGUAGE RecordWildCards #-}
 
 module GameOfLife
-  ( CellState(..)
-  , Board
-  , Position
-  , nextState
+  ( affectedCells
   , mkBoard
-  , changeStateOfCellAt
-  , stateOfCellAt
+  , nextGenBoard
   , countNeighboursOf
-  , affectedCellsOn
-  , nextGenerationOn
+  , nextGen
+  , CellState(..)
+  , fromLines
+  , cellStateAt
+  , setStateAt
+  , setMultipleAlive
+  , setMultipleDead
   ) where
 
-import           Data.Set (Set)
-import qualified Data.Set as Set
+import           Data.Maybe (mapMaybe)
+import           Data.Set   (Set)
+import qualified Data.Set   as Set
 
 data CellState
   = Living
   | Dead
-  deriving (Show, Eq)
+  deriving (Eq, Show)
 
 newtype Board = Board
   { livingCells :: Set Position
-  } deriving (Show, Eq)
+  } deriving (Eq, Show)
 
 type Position = (Int, Int)
-
-nextState :: CellState -> Int -> CellState
-nextState currentState 2 = currentState
-nextState _ 3            = Living
-nextState _ _            = Dead
 
 mkBoard :: Board
 mkBoard = Board {livingCells = Set.empty}
 
-stateOfCellAt :: Board -> Position -> CellState
-stateOfCellAt Board {..} position
-  | position `Set.member` livingCells = Living
+cellStateAt :: Board -> Position -> CellState
+cellStateAt Board {..} position
+  | position `elem` livingCells = Living
   | otherwise = Dead
 
-changeStateOfCellAt :: CellState -> Board -> Position -> Board
-changeStateOfCellAt Living Board {..} position = Board {livingCells = Set.insert position livingCells}
-changeStateOfCellAt Dead Board {..} position = Board {livingCells = Set.delete position livingCells}
+setStateAt :: Board -> Position -> CellState -> Board
+setStateAt Board {..} position Living = Board {livingCells = position `Set.insert` livingCells}
+setStateAt Board {..} position Dead = Board {livingCells = position `Set.delete` livingCells}
 
-countNeighboursOf :: Board -> Position -> Int
-countNeighboursOf board position = length livingNeighbours
+setMultipleAlive :: Board -> [Position] -> Board
+setMultipleAlive = foldl setAlive
+
+setMultipleDead :: Board -> [Position] -> Board
+setMultipleDead = foldl kill
+
+setAlive :: Board -> Position -> Board
+setAlive board position = setStateAt board position Living
+
+kill :: Board -> Position -> Board
+kill board position = setStateAt board position Dead
+
+nextGen :: CellState -> Int -> CellState
+nextGen current 2 = current
+nextGen _ 3       = Living
+nextGen _ _       = Dead
+
+affectedCells :: Board -> [Position]
+affectedCells Board {..} = onlyUnique $ surrounding =<< Set.elems livingCells
   where
-    livingNeighbours = filter isLiving candidates
-    isLiving position = board `stateOfCellAt` position == Living
-    candidates = neighboursOf position
+    onlyUnique = Set.toList . Set.fromList
+    surrounding position = position : neighboursOf position
 
 neighboursOf :: Position -> [Position]
 neighboursOf (x, y) =
   [(x - 1, y - 1), (x, y - 1), (x + 1, y - 1), (x - 1, y), (x + 1, y), (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)]
 
-affectedCellsOn :: Board -> [Position]
-affectedCellsOn Board {..} = filterDuplicates allNeighboursAndSelf
+countNeighboursOf :: Board -> Position -> Int
+countNeighboursOf board position = length . filter isAlive $ candidates
   where
-    filterDuplicates = Set.toList . Set.fromList
-    allNeighboursAndSelf = neighboursAndSelf =<< Set.toList livingCells
-    neighboursAndSelf position = position : neighboursOf position
+    candidates = neighboursOf position
+    isAlive position = board `cellStateAt` position == Living
 
-nextGenerationOn :: Board -> Board
-nextGenerationOn board = Board {livingCells = livingCellsInNextGeneration}
+fromLines :: [String] -> Board
+fromLines lines = foldl setState mkBoard cellPositions
   where
-    livingCellsInNextGeneration = Set.fromList . map fst . filter isLiving $ allPairsInNextGeneration
-    isLiving (_, state) = state == Living
-    allPairsInNextGeneration = map (pairInNextGeneration . pairPositionWithState) . affectedCellsOn $ board
-    pairInNextGeneration (position, currentState) =
-      (position, nextState currentState $ board `countNeighboursOf` position)
-    pairPositionWithState position = (position, board `stateOfCellAt` position)
+    setState board (state, pos) = setStateAt board pos state
+    positions = [(x, y) | y <- [0 .. maxY - 1], x <- [0 .. maxX - 1]]
+    maxY = length lines
+    maxX = length . head $ lines
+    cellPositions = zip cells positions
+    cells = map whichState $ concat lines
+    whichState '.' = Dead
+    whichState 'X' = Living
+
+nextGenBoard :: Board -> Board
+nextGenBoard board = Board {livingCells = Set.fromList livingCellsInNextGen}
+  where
+    livingCellsInNextGen = mapMaybe toNextGen . affectedCells $ board
+    toNextGen position
+      | nextState position == Living = Just position
+      | otherwise = Nothing
+    nextState position = nextGen (board `cellStateAt` position) (board `countNeighboursOf` position)
--- haskell/game-of-life/test/GameOfLifeSpec.hs
+++ haskell/game-of-life/test/GameOfLifeSpec.hs
@@ -2,131 +2,131 @@ module GameOfLifeSpec
   ( spec
   ) where
 
-import           GameOfLife
+import           Data.List         (delete)
+import           Data.Set          (Set)
+import qualified Data.Set          as Set
 import           Test.Hspec        (Spec, context, describe, it, shouldBe,
                                     shouldMatchList)
 import           Test.Hspec.Runner (hspec)
 
+import           GameOfLife
+
 main :: IO ()
 main = hspec spec
-
-setMultipleAlive :: [Position] -> Board
-setMultipleAlive = foldl (changeStateOfCellAt Living) mkBoard
 
 spec :: Spec
 spec =
   describe "Game of Life" $ do
-    describe "Next generation rules" $ do
-      let livingCell = Living
-      let deadCell = Dead
+    context "Rules for Next Generation" $ do
       context "Living Cell" $ do
-        it "should die on less than 2 living neighbours" $ do
-          nextState livingCell 0 `shouldBe` Dead
-          nextState livingCell 1 `shouldBe` Dead
-        it "should survive on 2 or 3 living neighbours" $ do
-          nextState livingCell 2 `shouldBe` Living
-          nextState livingCell 3 `shouldBe` Living
-        it "should die on more than 3 living neighbours" $ do
-          nextState livingCell 4 `shouldBe` Dead
-          nextState livingCell 8 `shouldBe` Dead
+        let livingCell = Living
+        it "Underpopulation" $ do
+          nextGen livingCell 0 `shouldBe` Dead
+          nextGen livingCell 1 `shouldBe` Dead
+        it "Survival" $ do
+          nextGen livingCell 2 `shouldBe` Living
+          nextGen livingCell 3 `shouldBe` Living
+        it "Overcrowding" $ do
+          nextGen livingCell 4 `shouldBe` Dead
+          nextGen livingCell 8 `shouldBe` Dead
       context "Dead Cell" $ do
-        it "should stay dead" $ do
-          nextState deadCell 1 `shouldBe` Dead
-          nextState deadCell 2 `shouldBe` Dead
-          nextState deadCell 4 `shouldBe` Dead
-        it "should be born on exactly 3 living neighbours" $ nextState deadCell 3 `shouldBe` Living
-    describe "Board" $ do
+        let deadCell = Dead
+        it "Stay Dead" $ do
+          nextGen deadCell 2 `shouldBe` Dead
+          nextGen deadCell 1 `shouldBe` Dead
+          nextGen deadCell 4 `shouldBe` Dead
+        it "Reproduction" $ nextGen deadCell 3 `shouldBe` Living
+    context "Board" $ do
       let emptyBoard = mkBoard
-      context "Querying" $ do
-        let boardWithOneLivingCell = changeStateOfCellAt Living emptyBoard (1, 1)
-        it "should have dead cells initially" $ emptyBoard `stateOfCellAt` (1, 1) `shouldBe` Dead
-        it "should allow cell to be set alive" $ boardWithOneLivingCell `stateOfCellAt` (1, 1) `shouldBe` Living
-        it "should allow cell to be set to dead" $ do
-          let notAliveAnymore = changeStateOfCellAt Dead boardWithOneLivingCell (1, 1)
-          notAliveAnymore `stateOfCellAt` (1, 1) `shouldBe` Dead
-        it "should memorize position of living cell" $ boardWithOneLivingCell `stateOfCellAt` (0, 0) `shouldBe` Dead
-        it "should memorize position of many living cells" $ do
-          let boardWithTwoLivingCells = changeStateOfCellAt Living boardWithOneLivingCell (0, 0)
-          boardWithTwoLivingCells `stateOfCellAt` (1, 1) `shouldBe` Living
-          boardWithTwoLivingCells `stateOfCellAt` (0, 0) `shouldBe` Living
+      context "Accessing" $ do
+        it "Has only dead cells" $ do
+          emptyBoard `cellStateAt` (0, 0) `shouldBe` Dead
+          emptyBoard `cellStateAt` (4, 1) `shouldBe` Dead
+        it "Has one living cell" $ do
+          let board = setStateAt emptyBoard (0, 0) Living
+          board `cellStateAt` (0, 0) `shouldBe` Living
+        it "Has multiple living cells" $ do
+          let board = setMultipleAlive emptyBoard [(0, 0), (1, 1), (2, 2)]
+          board `cellStateAt` (0, 0) `shouldBe` Living
+          board `cellStateAt` (1, 1) `shouldBe` Living
+          board `cellStateAt` (2, 2) `shouldBe` Living
+          board `cellStateAt` (4, 12) `shouldBe` Dead
+        it "Can kill a living cell" $ do
+          let board = setMultipleAlive emptyBoard [(0, 0)]
+          let killCell = setStateAt board (0, 0) Dead
+          killCell `cellStateAt` (0, 0) `shouldBe` Dead
+        it "Setting Alive is idempotent" $ do
+          let board = setMultipleAlive emptyBoard [(0, 0), (0, 0), (0, 0)]
+          let killCell = setStateAt board (0, 0) Dead
+          killCell `cellStateAt` (0, 0) `shouldBe` Dead
+        it "Killing is idempotent" $ do
+          let board = setMultipleAlive emptyBoard [(0, 0), (1, 1)]
+          let killCells = setMultipleDead board [(0, 0), (0, 0), (1, 1)]
+          killCells `cellStateAt` (0, 0) `shouldBe` Dead
+          killCells `cellStateAt` (1, 1) `shouldBe` Dead
       context "Counting Neighbours" $ do
-        it "should count on empty board" $ emptyBoard `countNeighboursOf` (1, 1) `shouldBe` 0
-        it "should count on board with living cells" $ do
-          let board = setMultipleAlive [(0, 0), (1, 0), (0, 1)]
-          board `countNeighboursOf` (1, 1) `shouldBe` 3
-        it "should only count adjacent cells" $ do
-          let board = setMultipleAlive [(3, 3)]
-          board `countNeighboursOf` (1, 1) `shouldBe` 0
-        it "should count on board with all neighbours alive" $ do
-          let cells = [(0, 0), (1, 0), (2, 0), (0, 1), (2, 1), (0, 2), (1, 2), (2, 2)]
-          let board = setMultipleAlive cells
-          board `countNeighboursOf` (1, 1) `shouldBe` 8
-        it "should not count the cell itself as its neighbour" $ do
-          let board = setMultipleAlive [(1, 1)]
-          board `countNeighboursOf` (1, 1) `shouldBe` 0
+        it "Counts empty board" $ countNeighboursOf emptyBoard (0, 0) `shouldBe` 0
+        it "Counts one living cell" $ do
+          let board = setMultipleAlive emptyBoard [(0, 0)]
+          countNeighboursOf board (1, 1) `shouldBe` 1
+        it "Ignores cell itself" $ do
+          let board = setMultipleAlive emptyBoard [(0, 0), (1, 1)]
+          countNeighboursOf board (1, 1) `shouldBe` 1
+        it "Counts all adjacent cells" $ do
+          let board =
+                setMultipleAlive emptyBoard [(0, 0), (1, 0), (2, 0), (0, 1), (1, 1), (2, 1), (0, 2), (1, 2), (2, 2)]
+          countNeighboursOf board (1, 1) `shouldBe` 8
+          countNeighboursOf board (2, 2) `shouldBe` 3
       context "Affected Cells" $ do
-        it "should return empty on empty board" $ affectedCellsOn emptyBoard `shouldBe` []
-        it "should return all neighbours of living cell" $ do
-          let oneLiving = setMultipleAlive [(1, 1)]
-          let expected = [(0, 0), (1, 0), (2, 0), (0, 1), (1, 1), (2, 1), (0, 2), (1, 2), (2, 2)]
-          affectedCellsOn oneLiving `shouldMatchList` expected
-        it "should return all neighbours of 2 distinct living cells" $ do
-          let twoLiving = setMultipleAlive [(1, 1), (4, 4)]
-          let expected =
-                [ (0, 0)
-                , (1, 0)
-                , (2, 0)
-                , (0, 1)
-                , (1, 1)
-                , (2, 1)
-                , (0, 2)
-                , (1, 2)
-                , (2, 2)
-                , (3, 3)
-                , (4, 3)
-                , (5, 3)
-                , (3, 4)
-                , (4, 4)
-                , (5, 4)
-                , (3, 5)
-                , (4, 5)
-                , (5, 5)
-                ]
-          affectedCellsOn twoLiving `shouldMatchList` expected
-        it "should return all neighbours of 2 adjacent living cells" $ do
-          let twoLiving = setMultipleAlive [(1, 1), (2, 2)]
-          let expected =
-                [ (0, 0)
-                , (1, 0)
-                , (2, 0)
-                , (0, 1)
-                , (1, 1)
-                , (2, 1)
-                , (0, 2)
-                , (1, 2)
-                , (2, 2)
-                , (3, 1)
-                , (3, 2)
-                , (1, 3)
-                , (2, 3)
-                , (3, 3)
-                ]
-          affectedCellsOn twoLiving `shouldMatchList` expected
-    describe "Next generation" $ do
-      let emptyBoard = mkBoard
-      it "should keep empty board empty" $
-        nextGenerationOn emptyBoard `shouldBe` emptyBoard
-      it "should let single cell die" $ do
-        let oneLiving = setMultipleAlive [(1, 1)]
-        nextGenerationOn oneLiving `shouldBe` emptyBoard
-      it "should keep 2x2 square stable" $ do
-        let square2x2 = setMultipleAlive [(0, 1), (1, 1), (0, 2), (1, 2)]
-        nextGenerationOn square2x2 `shouldBe` square2x2
-      it "should flip 1x3 block" $ do
-        let block1x3 = setMultipleAlive [(1, 1), (1, 2), (1, 3)]
-        let expected = setMultipleAlive [(0, 2), (1, 2), (2, 2)]
-        nextGenerationOn block1x3 `shouldBe` expected
-      it "should flip 3x1 block" $ do
-        let block3x1 = setMultipleAlive [(0, 2), (1, 2), (2, 2)]
-        let expected = setMultipleAlive [(1, 1), (1, 2), (1, 3)]
-        nextGenerationOn block3x1 `shouldBe` expected
+        it "None Affected" $ affectedCells emptyBoard `shouldBe` []
+        it "Single Living Cell" $ do
+          let board = setMultipleAlive emptyBoard [(1, 1)]
+          affectedCells board `shouldMatchList` [(0, 0), (1, 0), (2, 0), (0, 1), (1, 1), (2, 1), (0, 2), (1, 2), (2, 2)]
+        it "Multiple Living Cells" $ do
+          let board = setMultipleAlive emptyBoard [(1, 1), (3, 3)]
+          affectedCells board `shouldMatchList`
+            [ (0, 0)
+            , (1, 0)
+            , (2, 0)
+            , (0, 1)
+            , (1, 1)
+            , (2, 1)
+            , (0, 2)
+            , (1, 2)
+            , (2, 2)
+            , (3, 2)
+            , (4, 2)
+            , (2, 3)
+            , (3, 3)
+            , (4, 3)
+            , (2, 4)
+            , (3, 4)
+            , (4, 4)
+            ]
+      context "Initializing Board" $ do
+        it "Empty" $
+          fromLines [] `shouldBe` mkBoard
+        it "Single Alive" $ do
+          let initialized = fromLines [".X."]
+          let expected = setMultipleAlive mkBoard [(1, 0)]
+          initialized `shouldBe` expected
+        it "Multiple Alive" $ do
+          let initialized = fromLines [".X.", ".X.", ".X."]
+          let expected = setMultipleAlive mkBoard [(1, 0), (1, 1), (1, 2)]
+          initialized `shouldBe` expected
+      context "Playing Game" $ do
+        it "Oscillator" $ do
+          let verticalBar = fromLines [".X.", ".X.", ".X."]
+          let horizontalBar = fromLines ["...", "XXX", "..."]
+          nextGenBoard verticalBar `shouldBe` horizontalBar
+          nextGenBoard horizontalBar `shouldBe` verticalBar
+        it "Square" $ do
+          let square = fromLines ["....", ".XX.", ".XX.", "...."]
+          nextGenBoard square `shouldBe` square
+        it "Diamond" $ do
+          let diamond = fromLines [".X.", "X.X", ".X."]
+          nextGenBoard diamond `shouldBe` diamond
+        it "Single" $ do
+          let single = fromLines ["...", ".X.", "..."]
+          let empty = fromLines []
+          nextGenBoard single `shouldBe` empty
