2848d6d11bd83686710c57065f7014b08b8b3a8d
Christoph Welcz <christoph@welcz.de>
2018-12-05 20:03:45 +0100

    configuring which mongo to run by properties instead of weird profile class

--- spring/mongo/src/main/kotlin/de/welcz/springmongo/Application.kt
+++ spring/mongo/src/main/kotlin/de/welcz/springmongo/Application.kt
@@ -1,16 +1,14 @@
 package de.welcz.springmongo
 
-import de.welcz.springmongo.entity.MessageDoc
 import de.welcz.springmongo.control.MessagesRepository
+import de.welcz.springmongo.entity.MessageDoc
 import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 
-@SpringBootApplication(exclude = [EmbeddedMongoAutoConfiguration::class])
+@SpringBootApplication
 class Application {
     private val log = LoggerFactory.getLogger(Application::class.java)
 
