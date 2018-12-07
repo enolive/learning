d1376547aa85f3f2dc49e236efad7db39793003c
Christoph Welcz <christoph@welcz.de>
2018-12-07 20:23:50 +0100

    basic setup

---
+++ spring/fizz-buzz/src/main/java/de/welcz/fizzbuzz/FizzBuzzApplication.java
@@ -0,0 +1,12 @@
+package de.welcz.fizzbuzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzBuzzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FizzBuzzApplication.class, args);
+    }
+}
