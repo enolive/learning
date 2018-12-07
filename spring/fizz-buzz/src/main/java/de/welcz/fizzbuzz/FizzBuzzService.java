483204a3fc54e146368d0f54b1276e28d9160ceb
Christoph Welcz <christoph@welcz.de>
2018-12-07 20:39:42 +0100

    basic rules

--- spring/fizz-buzz/src/main/java/de/welcz/fizzbuzz/FizzBuzzService.java
+++ spring/fizz-buzz/src/main/java/de/welcz/fizzbuzz/FizzBuzzService.java
@@ -5,6 +5,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class FizzBuzzService {
     String calculate(int input) {
-        return null;
+        if (isDivisibleBy(input, 3)) {
+            return "Fizz";
+        }
+        if (isDivisibleBy(input, 5)) {
+            return "Buzz";
+        }
+        return String.valueOf(input);
+    }
+
+    private boolean isDivisibleBy(int input, int divisor) {
+        return input % divisor == 0;
     }
 }
---
+++ spring/fizz-buzz/src/test/java/de/welcz/fizzbuzz/FizzBuzzServiceTest.java
@@ -0,0 +1,39 @@
+package de.welcz.fizzbuzz;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+class FizzBuzzServiceTest {
+    @Autowired
+    private FizzBuzzService service;
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1, 1",
+            "2, 2",
+    })
+    void replicates_normal_numbers(int input, String expected) {
+        assertThat(service.calculate(input)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {3, 6})
+    void fizz_numbers_divisible_by_3(int input) {
+        assertThat(service.calculate(input)).isEqualTo("Fizz");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {5, 10})
+    void buzz_numbers_divisible_by_5(int input) {
+        assertThat(service.calculate(input)).isEqualTo("Buzz");
+    }
+}
