60b2aba9be2127a7b14939577c6b403d4278d1d9
Christoph Welcz <christoph@welcz.de>
2019-05-29 01:24:57 +0200

    migrated test to spring boot test

--- spring/async-services/src/test/java/de/welcz/asyncservices/ReactiveServiceTest.java
+++ spring/async-services/src/test/java/de/welcz/asyncservices/ReactiveServiceTest.java
@@ -1,15 +1,23 @@
 package de.welcz.asyncservices;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.concurrent.ExecutionException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class ReactiveServiceTest {
+  @Autowired
+  private ReactiveService service;
+
   @Test
   public void sayHelloShouldWork() throws ExecutionException, InterruptedException {
-    ReactiveService service = new ReactiveService();
     String result = service.sayHelloToAsync("World").get();
     assertThat(result).isEqualTo("Hello, World!");
   }
