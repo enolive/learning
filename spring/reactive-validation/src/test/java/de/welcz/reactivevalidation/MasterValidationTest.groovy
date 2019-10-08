91ae1a59f6f6c11893eb1e84cc4aa8b6e9195e44
Christoph Welcz <christoph@welcz.de>
2019-10-08 21:40:21 +0200

    oops

--- spring/reactive-validation/src/test/java/de/welcz/reactivevalidation/MasterValidationTest.groovy
+++ spring/reactive-validation/src/test/java/de/welcz/reactivevalidation/MasterValidationTest.groovy
@@ -4,16 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.validation.ValidationAutoConfiguration
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ContextConfiguration
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean
 import spock.lang.Specification
 
 import javax.validation.ConstraintViolation
+import javax.validation.Validator
 
 @SpringBootTest(classes = ValidationAutoConfiguration)
 @ContextConfiguration(classes = [])
 class MasterValidationTest extends Specification {
     @Autowired
-    private LocalValidatorFactoryBean validator
+    private Validator validator
 
     def "validation works"() {
         given: 'an invalid object'
