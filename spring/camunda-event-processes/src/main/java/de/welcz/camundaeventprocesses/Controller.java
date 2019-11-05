507ad1ffadd2ef661cd640515d22009733cf72aa
Christoph Welcz <christoph@welcz.de>
2019-11-06 00:23:57 +0100

    implemented sending to a specific execution by the power of flux/vavr

--- spring/camunda-event-processes/src/main/java/de/welcz/camundaeventprocesses/Controller.java
+++ spring/camunda-event-processes/src/main/java/de/welcz/camundaeventprocesses/Controller.java
@@ -1,15 +1,28 @@
 package de.welcz.camundaeventprocesses;
 
+import io.vavr.collection.HashMap;
+import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.runtime.EventSubscription;
+import org.camunda.bpm.engine.runtime.Execution;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.security.SecureRandom;
+import java.text.MessageFormat;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 @RestController
+@Slf4j
 public class Controller {
   private final RuntimeService runtimeService;
 
@@ -34,16 +47,61 @@ public class Controller {
     runtimeService.createMessageCorrelation("ReceiveHellosMessage")
                   .processInstanceBusinessKey(key)
                   .setVariable("sub-process", processId)
-                  .correlate();
+                  .correlateAll();
     return processId;
   }
 
-  @PostMapping("/hellos/{key}/greetings/{name}")
-  public void showGreeting(@PathVariable String key, @PathVariable String name) {
-    runtimeService.createMessageCorrelation("NameMessage")
-                  .processInstanceBusinessKey(key)
-                  .setVariable("name", name)
-                  .correlateAll();
+  @PostMapping("/hellos/{key}/greetings/names/{name}")
+  public String showGreeting(@PathVariable String key, @PathVariable String name) {
+    final var results = runtimeService.createMessageCorrelation("NameMessage")
+                                      .processInstanceBusinessKey(key)
+                                      .setVariable("name", name)
+                                      .correlateAllWithResult();
+    return MessageFormat.format("Greetings shown to {0}", results.size());
+  }
+
+  @PostMapping("/hellos/{key}/greetings/{subProcess}/names/{name}")
+  public void showGreetingsForSpecificProcess(@PathVariable String key,
+                                              @PathVariable int subProcess,
+                                              @PathVariable String name) {
+
+    findProcessInstanceIdBy(key).flatMap(findSubscriptionToNameMessage(subProcess))
+                                .doOnNext(sendNameMessage(name))
+                                .subscribe();
+  }
+
+  private Function<String, Mono<EventSubscription>> findSubscriptionToNameMessage(int subProcess) {
+    return id -> Flux.fromIterable(runtimeService.createEventSubscriptionQuery()
+                                                 .eventName("NameMessage")
+                                                 .list())
+                     .filter(hasVariableWithValue("sub-process-local", subProcess))
+                     .next();
+  }
+
+  private Mono<String> findProcessInstanceIdBy(String key) {
+    return Flux.fromIterable(runtimeService.createProcessInstanceQuery()
+                                           .processInstanceBusinessKey(key)
+                                           .list())
+               .map(Execution::getProcessInstanceId)
+               .next();
+  }
+
+  private Predicate<EventSubscription> hasVariableWithValue(
+      @SuppressWarnings("SameParameterValue") String variableName,
+      Object expectedValue) {
+    return subscription -> Objects.equals(
+        runtimeService.getVariable(subscription.getExecutionId(), variableName),
+        expectedValue);
+  }
+
+  private Consumer<EventSubscription> sendNameMessage(String name) {
+    return subscription -> runtimeService.messageEventReceived("NameMessage",
+                                                               subscription.getExecutionId(),
+                                                               nameAsMap(name));
+  }
+
+  private Map<String, Object> nameAsMap(String name) {
+    return HashMap.<String, Object>of("name", name).toJavaMap();
   }
 
   private int randomNumber() {
