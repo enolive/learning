278dcb584474ba70faf312cdb30f522877928e9b
Christoph Welcz <christoph@welcz.de>
2022-08-28 08:43:13 +0200

    new version of the tennis kata

--- angular/chart-mock/jest.config.js
+++ angular/chart-mock/jest.config.js
@@ -1,8 +1,10 @@
 module.exports = {
+  preset: 'jest-preset-angular',
+  globalSetup: 'jest-preset-angular/global-setup',
   setupFilesAfterEnv: [
     '<rootDir>/setup-jest.ts'
   ],
   moduleNameMapper: {
     "^lodash-es$": "lodash"
-  }
+  },
 }
